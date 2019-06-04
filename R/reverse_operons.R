6db927db49ffe464459c98b1ba41fbc44a97ee17
Samuel Chen <chensam2@msu.edu>
2019-06-04 09:38:52 -0400

    Changed reverse_operons to a function

--- R/reverse_operons.R
+++ R/reverse_operons.R
@@ -1,65 +1,71 @@
 library(tidyverse)
 
-prot <- read_tsv("data/pspa_snf7.txt")
-#operon straightner
-opvec=prot$GenContext   #operon column from a df containing cleanblastclust columns
-opvec=gsub(pattern = ">",replacement = ">|",x = opvec)
-opvec=gsub(pattern = "<",replacement = "|<",x = opvec)
-opvec=gsub(pattern = "\\|\\|",replacement = "\\|=\\|",x = opvec)
+#'Reverse Operons
+#'
+#'Reverses the direction of the operons
+#'
+#'This function reverses the direction of operons in a column of a data frame called 'GenContext'. Returns
+#'the original dataframe with the original operons in a column called 'GenContext.old' and the reversed
+#'operons in a column called 'GenContext'
+#'
+#'@param prot A data frame containing a GenContext column
+#'@examples reverse_operons(pspa.sub)
+reverse_operons <- function(prot){
+  #operon straightner
+  opvec <- prot$GenContext.norep   #operon column from a df containing cleanblastclust columns
+  opvec <- gsub(pattern = ">",replacement = ">|",x = opvec)%>%
+    gsub(pattern = "<",replacement = "|<") %>%
+    gsub(pattern = "\\|\\|",replacement = "\\|=\\|")
 
-op.list=strsplit(x = opvec, split =  "\\|")
-if(any(is.na(op.list))) op.list[[which(is.na(op.list))]]="-"
-op.list=lapply(1:length(op.list), function(x) {if(any(op.list[[x]]=="")) op.list[[x]][which(op.list[[x]] !="")] else op.list[[x]] })
+  #op.list takes opvec and for each element in opvec, a list of characters are created by spliting the strings
+  op.list <- strsplit(x = opvec, split =  "\\|")%>%
+    replace_na("-")
+  #Removes empty strings from the list
+  op.list <- map(1:length(op.list),
+                 function(x) {
+                   if(any(op.list[[x]]=="")) op.list[[x]][which(op.list[[x]] !="")]
+                   else op.list[[x]] })
+  #te is the
+  te <- map(1:length(op.list),
+            function(x) op.list[[x]][grep("\\*", op.list[[x]])])
+  torev <- grep("^<",te)
 
-te=lapply(1:length(op.list), function(x) op.list[[x]][grep("\\*", op.list[[x]])])
-ye=unlist(lapply(te, function(x) substr(x[1],1,1)))
-torev=which(ye=="<")
+  te <- op.list[torev]%>%
+    map( function(x) gsub(pattern = "<-|->", replacement = "",x = x)) %>%
+    map(rev)
+  witheq <- grep(pattern = "=",x = te)
+  withouteq <- which(!((1:length(te)) %in% witheq))
+  ge <- te[witheq]
 
-te=op.list[torev]
-te=lapply(te, function(x) gsub(pattern = "<-|->", replacement = "",x = x))
-te=lapply(te, rev)
-witheq=grep(pattern = "=",x = te)
-withouteq=which(!((1:length(te)) %in% witheq))
-ge=te[witheq]
-
-reveql=function(x){
-  w=x
-  y=rep(NA, length(w))
-  d=1
-  for(j in 1:length(w)){
-    if(w[j]=="=") d= d*(-1)
-    if(d==1 && w[j] != "=") {y[j]=paste(w[j],"->", sep = "")
-    } else if (d==-1 && w[j] != "="){
-      y[j]=paste("<-",w[j], sep = "")
-    } else{
-      y[j]="="
+  reveql=function(x){
+    w <- x
+    y <- rep(NA, length(w))
+    d <- 1
+    for(j in 1:length(w)){
+      if(w[j]=="=") d <- d*(-1)
+      if(d==1 && w[j] != "=") {y[j]=paste(w[j],"->", sep = "")
+      } else if (d==-1 && w[j] != "="){
+        y[j] <- paste("<-",w[j], sep = "")
+      } else{
+        y[j] <- "="
+      }
     }
+    return(y)
   }
-  return(y)
+
+  ge <- map(1:length(ge), function(x) reveql(ge[[x]]))
+  ye <- te[withouteq]
+  ye <- map(1:length(ye), function(x) unname(sapply(ye[[x]], function(y) paste(y,"->", sep = ""))))
+
+  te[witheq] <- ge
+  te[withouteq]<-ye
+  op.list[torev] <- te
+
+  revopvec <- unlist(map(op.list, function(x) paste(x, collapse = "")))%>%
+    gsub(pattern = "=",replacement = "\\|\\|") %>%
+    tibble::enframe(name = NULL)
+  colnames(revopvec) <- (c("GenContext.norep"))
+  names(prot)[names(prot)=='GenContext.norep'] <- "GenContext.norep.old"
+  return(cbind(prot,revopvec))
 }
 
-ge=lapply(1:length(ge), function(x) reveql(ge[[x]]))
-ye=te[withouteq]
-ye=lapply(1:length(ye), function(x) unname(sapply(ye[[x]], function(y) paste(y,"->", sep = ""))))
-
-te[witheq]=ge
-te[withouteq]=ye
-op.list[torev]=te
-
-revopvec=unlist(lapply(op.list, function(x) paste(x, collapse = "")))
-revopvec=gsub(pattern = "=",replacement = "\\|\\|",revopvec)
-
-#common core of a set of operons
-te=revopvec
-te=gsub(pattern = ">",replacement = ">|",x = te)
-te=gsub(pattern = "<",replacement = "|<",x = te)
-te=gsub(pattern = "\\|\\|",replacement = "\\|=\\|",x = te)
-op.list=strsplit(x = te, split =  "\\|")
-
-Reduce(intersect, op.list[1:5])
-
-#operon types
-ye=sort(table(revopvec), decreasing = T)
-sink("operon.table")
-knitr::kable(ye,digits = 4,format = "pandoc",col.names = c("Domain", "count") )
-sink()
