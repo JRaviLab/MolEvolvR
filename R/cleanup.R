59a5a49c569b23871384edf4dd62d4d3efcb5aff
Samuel Chen <chensam2@msu.edu>
2019-06-10 13:47:05 -0400

    Updated cleanup functions and the-process

--- R/cleanup.R
+++ R/cleanup.R
@@ -20,23 +20,24 @@ colnames.op_ins_cls <- function(){
 #### CLEANUP FUNCTIONS ####
 ###########################
 ## Function to cleanup Species
-species.cleanup <- function(x) {
-	x %>%
-		str_replace_all(coll("sp. ", TRUE), "sp ") %>%
-		str_replace_all(coll("str. ", TRUE), "str ") %>%
-		str_replace_all(coll(" = ", TRUE), " ") %>%
-		str_replace_all(coll("-", TRUE), "") %>%
-		str_replace_all(coll(".", TRUE), "") %>%
-		str_replace_all(coll("(", TRUE), "") %>%
-		str_replace_all(coll(")", TRUE), "") %>%
-		str_replace_all(coll("[", TRUE), "") %>%
-		str_replace_all(coll("]", TRUE), "") %>%
-		str_replace_all(coll("\\", TRUE), "") %>%
-		str_replace_all(coll("/", TRUE), "") %>%
-		str_replace_all(coll("\'", TRUE), "") %>%
-		str_replace_all(coll("  ", TRUE), " ")
-}
+# species.cleanup <- function(x) {
+# 	x %>%
+# 		str_replace_all(coll("sp. ", TRUE), "sp ") %>%
+# 		str_replace_all(coll("str. ", TRUE), "str ") %>%
+# 		str_replace_all(coll(" = ", TRUE), " ") %>%
+# 		str_replace_all(coll("-", TRUE), "") %>%
+# 		str_replace_all(coll(".", TRUE), "") %>%
+# 		str_replace_all(coll("(", TRUE), "") %>%
+# 		str_replace_all(coll(")", TRUE), "") %>%
+# 		str_replace_all(coll("[", TRUE), "") %>%
+# 		str_replace_all(coll("]", TRUE), "") %>%
+# 		str_replace_all(coll("\\", TRUE), "") %>%
+# 		str_replace_all(coll("/", TRUE), "") %>%
+# 		str_replace_all(coll("\'", TRUE), "") %>%
+# 		str_replace_all(coll("  ", TRUE), " ")
+# }
 ## Function to cleanup Toast-rack names
+##
 # replace.toastrack <- function(x) {
 # 	x %>%
 # 		str_replace_all(coll("toast-rack", TRUE), "Toast-rack") %>%
@@ -45,26 +46,34 @@ species.cleanup <- function(x) {
 # 		str_replace_all("DUF2154", "Toast-rack") %>%
 # 		str_replace_all("DUF2807", "Toast-rack") %>%
 # 		str_replace_all("DUF2157", "Toast_rack_N")
+# str_replace_all("ASH-IG", "IG")
 # }
 
+#use in
 ## Function to deal with REPEATED DOMAINS with (s)!!!
-repeats2s <- function(x){
-	x %>%
-		str_replace_all("\\+", " ") %>%
-		str_replace_all("(?i)\\b([a-z0-9_-]+)\\b(?:\\s+\\1\\b)+", "\\1(s)") %>%
-		str_replace_all(" ", "+")
-}
+# repeats2s <- function(x){
+# 	x %>%
+# 		str_replace_all("\\+", " ") %>%
+# 		str_replace_all("(?i)\\b([a-z0-9_-]+)\\b(?:\\s+\\1\\b)+", "\\1(s)") %>%
+# 		str_replace_all(" ", "+")
+# }
+
+## Remove extra pluses that may occur from removal
+##New file
 ## Function to remove spurious domains (from ignore.list) | DomArch column !!!
 ## ALERT !!! See if this creates problems!!!
-remove.toastrack.ignored.doms <- function(x) {
-	x %>%
-		str_replace_all("EP1\\+","") %>%
-		str_replace_all("EIID-AGA\\+","") %>%
-		str_replace_all("LPD29\\+","") %>%
-		str_replace_all("Imm3\\+","") %>%
-		# str_replace_all("FilH","") %>%
-		str_replace_all("ASH-IG", "IG")
-}
+# remove.toastrack.ignored.doms <- function(x) {
+# 	x %>%
+# 		str_replace_all("EP1\\+","") %>%
+# 		str_replace_all("EIID-AGA\\+","") %>%
+# 		str_replace_all("LPD29\\+","") %>%
+# 		str_replace_all("Imm3\\+","")
+#     #%>%
+# 		# str_replace_all("FilH","") %>%
+#
+# }
+
+
 ## Function to replace singleton domains with the (s) form
 domarch.convert2s.forupset <- function(x){ x %>%
 		str_replace_all("^TM\\+","TM(s)+") %>%
@@ -74,110 +83,97 @@ domarch.convert2s.forupset <- function(x){ x %>%
 		# str_replace_all("PspC$","PspC(s)")
 }
 ## Function to remove empty rows
-remove.empty.rows <- function(x){	x %>%
-		as_tibble() %>%
-		filter(grepl("\\*", GenContext.norep)) %>%		# Keep only rows with Query*
-		filter(!grepl("^-$", GenContext.norep)) %>%		# remove "-"
-		filter(!grepl("^NA$", GenContext.norep)) %>%	# remove "NA"
-		filter(!grepl("^$", GenContext.norep)) #%>%		# remove empty rows
-		# filter(!grepl("^-$", DomArch.norep)) %>%			# remove "-"
-		# filter(!grepl("^NA$", DomArch.norep)) %>%			# remove "NA"
-		# filter(!grepl("^$", DomArch.norep))						# remove empty rows
+# remove.empty.rows <- function(x){	x %>%
+# 		as_tibble() %>%
+# 		filter(grepl("\\*", GenContext.norep)) %>%		# Keep only rows with Query*
+# 		filter(!grepl("^-$", GenContext.norep)) %>%		# remove "-"
+# 		filter(!grepl("^NA$", GenContext.norep)) %>%	# remove "NA"
+# 		filter(!grepl("^$", GenContext.norep)) #%>%		# remove empty rows
+# 		# filter(!grepl("^-$", DomArch.norep)) %>%			# remove "-"
+# 		# filter(!grepl("^NA$", DomArch.norep)) %>%			# remove "NA"
+# 		# filter(!grepl("^$", DomArch.norep))						# remove empty rows
+# }
+
+
+
+#'Cleanup Species
+#'
+#'Cleans up the species column of a data frame by removing certain characters and rows.
+#'
+#'This function removes unneccessary characters from the 'Species' column.
+#'Certain rows may also removed from the table based on values in the 'GenContext.norep' column.
+#'A cleaned up version of the data table is returned.
+#'
+#'@param prot A data frame that contains columns 'Species' and 'GenContext.norep'
+#'@param remove_empty Boolean. If TRUE, rows with empty/unnecessary values in 'GenContext.norep' are removed
+#'@examples cleanup_species(pspa.sub,TRUE)
+cleanup_species <- function(prot,remove_empty = FALSE){
+  #FUNCTIONS CALLED HERE, if else might be better since only two options, T and F
+  prot$Species <- prot$Species %>%
+    str_replace_all(coll("sp. ", TRUE), "sp ") %>%
+    str_replace_all(coll("str. ", TRUE), "str ") %>%
+    str_replace_all(coll(" = ", TRUE), " ") %>%
+    str_replace_all(coll("-", TRUE), "") %>%
+    str_replace_all(coll(".", TRUE), "") %>%
+    str_replace_all(coll("(", TRUE), "") %>%
+    str_replace_all(coll(")", TRUE), "") %>%
+    str_replace_all(coll("[", TRUE), "") %>%
+    str_replace_all(coll("]", TRUE), "") %>%
+    str_replace_all(coll("\\", TRUE), "") %>%
+    str_replace_all(coll("/", TRUE), "") %>%
+    str_replace_all(coll("\'", TRUE), "") %>%
+    str_replace_all(coll("  ", TRUE), " ")
+  if(remove_empty){
+    prot <- prot%>%
+      as_tibble() %>%
+      filter(grepl("\\*", GenContext.norep)) %>%		# Keep only rows with Query*
+      filter(!grepl("^-$", GenContext.norep)) %>%		# remove "-"
+      filter(!grepl("^NA$", GenContext.norep)) %>%	# remove "NA"
+      filter(!grepl("^$", GenContext.norep)) #%>%		# remove empty rows
+  }
+  return(prot)
 }
-
-
-
-
-#Function that applies both remove.empty.rows and species.cleanup to a data table
-cleanup_species <- function(prot){
-  prot$Species <- species.cleanup(prot$Species)
-  return(remove.empty.rows(prot))
-
-  # #species.cleanup
-  # prot$Species %>%
-  #   str_replace_all(coll("sp. ", TRUE), "sp ") %>%
-  #   str_replace_all(coll("str. ", TRUE), "str ") %>%
-  #   str_replace_all(coll(" = ", TRUE), " ") %>%
-  #   str_replace_all(coll("-", TRUE), "") %>%
-  #   str_replace_all(coll(".", TRUE), "") %>%
-  #   str_replace_all(coll("(", TRUE), "") %>%
-  #   str_replace_all(coll(")", TRUE), "") %>%
-  #   str_replace_all(coll("[", TRUE), "") %>%
-  #   str_replace_all(coll("]", TRUE), "") %>%
-  #   str_replace_all(coll("\\", TRUE), "") %>%
-  #   str_replace_all(coll("/", TRUE), "") %>%
-  #   str_replace_all(coll("\'", TRUE), "") %>%
-  #   str_replace_all(coll("  ", TRUE), " ")
-  #   #Remove.empty.rows
-  # prot %>%
-  #   as_tibble() %>%
-  #   filter(grepl("\\*", GenContext.norep)) %>%		# Keep only rows with Query*
-  #   filter(!grepl("^-$", GenContext.norep)) %>%		# remove "-"
-  #   filter(!grepl("^NA$", GenContext.norep)) %>%	# remove "NA"
-  #   filter(!grepl("^$", GenContext.norep)) #%>%		# remove empty rows
-  # return(prot)
-}
-
+#Switch case for remove.empty.rows, check efficiency
+#Don't call other psp functions within these functions
 
 #'Cleaning Domain Architecture
 #'
 #'Cleans the DomArch column by replacing/removing certain domains
 #'
 #'This function cleans the DomArch column of one data frame by renaming certain domains according to a second data frame.
-#'Additionally, repeated domains are condensed into the name of the domain with (s) appended. The original data
-#'frame is returned with the clean DomArchs column and the old domains in the DomArchs.old column.
+#'Certain domains can be removed according to an additional data frame.
+#'The original data frame is returned with the clean DomArchs column and the old domains in the DomArchs.old column.
 #'
-#'@param eDNA_data A data frame containing a 'DomArch' column
+#'@param prot A data frame containing a 'DomArch' column
 #'@param domains_rename A data frame containing the domain names to be replaced in a column 'old' and the corresponding replacement
 #'values in a column 'new'
-#'@examples replace_doms(pspa.sub,pspdomains.rename)
-replace_doms <- function(eDNA_data,domains_rename){
-  DomArch.old <- eDNA_data$DomArch
-  for (j in 1:length(domains_rename$old)) {
+#'@param domains_remove A data frame containing the domain names to be removed in a column called 'domains'
+#'@examples replace_doms(pspa.sub,domains.replace,domains.remove)
+replace_doms <- function(prot,domains_rename, domains_remove){
+  DomArch.old <- prot$DomArch
 
-    #Find and replace the occurances of domain where it starts with domain+
-    regterm=paste ("^",domains_rename$old[j],"\\+",sep="")
-    #repterm is the string "domofnew+"
-    repterm=paste (domains_rename$new[j],"\\+",sep="")
-    #All indeces in eDNA_data$arch that start with regterm, are replaced with repterm
-    eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)]=gsub(regterm, repterm,x = eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)])
-
-    #Find and replace the occurances of domain where it ends in +domain
-    regterm=paste ("\\+",domains_rename$old[j],"$",sep="")
-    #repterm is now "+domainofnew" (very similar to first repterm)
-    repterm=paste ("\\+",domains_rename$new[j],sep="")
-    #All indeces in eDNA_data$arch that match second regterm are replaced with second repterm
-    eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)]=gsub(regterm, repterm,x = eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)])
-
-    #What if domain is replaced by "" -- there would be nothing there
-    #regterm3 = "+domainofold+"
-    regterm=paste ("\\+",domains_rename$old[j],"\\+",sep="")
-    repterm=paste ("\\+",domains_rename$new[j],"\\+",sep="")
-    eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)]=gsub(regterm, repterm,x = eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)])
-
-    # #all occurances where domain to be renamed occurs by itself is replaced
-    # regterm=paste (domains_rename$old[j],sep="")
-    # repterm=paste (domains_rename$new[j],sep="")
-    # eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)]=gsub(regterm, repterm,x = eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)])
-
-    #repterm= domains_rename$new[j]
-    #What does[1] do?
-    #While loop b/c it can appear multiple times
-    #while iterates keeps going until no more strings in eDNA_data$arch match regterm
-    while ( length(eDNA_data$DomArch[grep(regterm[1], eDNA_data$DomArch)]) > 0) {
-      #replace the regterm with repterm
-      eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)]=gsub(regterm, repterm,x = eDNA_data$DomArch[grep(regterm, eDNA_data$DomArch)])
-
-    }
-
+  #replace domains based on the domains_rename list
+  for(j in 1:length(domains_rename$old)){
+    prot$DomArch <- str_replace_all(prot$DomArch,as.vector(domains_rename$old[j]),as.vector(domains_rename$new[j]))
   }
+  #remove domains based on the domains_remove list
+  for(j in 1:length(as.vector(domains_remove$domains))){
+    prot$DomArch <- str_remove_all(prot$DomArch,as.vector(domains_remove$domains[j]))
+  }
+  #remove '+' at the start and end, as well as consecuative '+'
+  prot$DomArch <- gsub("\\++\\+","\\+",prot$DomArch)
+  prot$DomArch <- gsub("^\\+","",prot$DomArch)
+  prot$DomArch <- gsub("\\+$","",prot$DomArch)
 
-  eDNA_data$DomArch <- repeats2s(eDNA_data$DomArch) %>%
-    domarch.convert2s.forupset()
-  #eDNA_data$DomArch <- domarch.convert2s.forupset(eDNA_data$DomArch)
-  return(cbind(eDNA_data,DomArch.old))
+  # Deal with REPEATED DOMAINS by replacing with (s)!!
+  prot$DomArch <- prot$DomArch %>%
+    str_replace_all("\\+", " ") %>%
+    str_replace_all("(?i)\\b([a-z0-9_-]+)\\b(?:\\s+\\1\\b)+", "\\1(s)") %>%
+    str_replace_all(" ", "+")
+
+
+  return(cbind(prot,DomArch.old))
 }
-
 
 
 ##################################
