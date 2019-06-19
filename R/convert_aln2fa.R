f632d784d63baae46f569935c6371895f8795d60
Samuel Chen <chensam2@msu.edu>
2019-06-19 14:26:02 -0400

    Completed/fixed functions for phylogeny

--- R/convert_aln2fa.R
+++ R/convert_aln2fa.R
@@ -1,11 +1,18 @@
 # Created: April 19, 2019
 
-# library(tidyverse)
+library(tidyverse)
+library(data.table)
+library(seqRFLP)
+source("R/add_leaves.R")
 # library(here)
 # library(roxygen2)
 # library(docstring)
 
-convert_aln2fa <- function(input_file, lin_file) {
+
+convert_aln2fa <- function(input_file = "data/alignments/pspa_snf7.aln",
+                           lin_file = "data/pspa_snf7.txt",
+                           output_path = NULL,
+                           reduced = FALSE) {
     #' Adding Leaves to an alignment file w/ accessions
     #' @author Janani Ravi
     #' @keywords alignment, accnum, leaves, lineage, species
@@ -17,6 +24,10 @@ convert_aln2fa <- function(input_file, lin_file) {
     #' @param lin_file Character. Path to file. Protein file with accession +
     #' number to lineage mapping.
     #' Default is 'pspa.txt'
+    #' @param output_path Character. Path to the writen fasta file.
+    #' Default is 'NULL'
+    #' @param reduced Boolean. If TRUE, the fasta file will contain only one sequence per lineage.
+    #' Default is 'FALSE'
     #' @examples add_leaves('pspa_snf7.aln', 'pspa.txt')
     #' @details The alignment file would need two columns: 1. accession +
     #' number and 2. alignment. The protein homolog accession to lineage mapping +
@@ -24,7 +35,31 @@ convert_aln2fa <- function(input_file, lin_file) {
     #' @note Please refer to the source code if you have alternate +
     #' file formats and/or column names.
 
-    lin <- lin_file %>% 
-    select(lineage) %>%
-    strsplit()
+    aln <- add_leaves(input_file,lin_file,reduced)
+    names <- aln$Leaf_Acc
+    sequences <- aln$Alignment
+    aln <- data.table(names,sequences)
+    fasta_file <- dataframe2fas(aln, output_path)
+    return(fasta_file)
 }
+
+
+# #takes in a datatable, not a file
+# convert_to_fasta <- function(aln,output_file = NULL){
+#   names <- aln$Leaf_Acc
+#   sequences <- aln$Alignment
+#   #library(data.table)
+#   aln <- data.table(names,sequences)
+#   #library(seqRFLP)
+#   fasta_file <- dataframe2fas(aln, output_file)
+#   return(fasta_file)
+# }
+
+
+
+
+# aln2tsv <- function(file_path){
+#   cfile <- read_delim("data/alignments/pspc.gismo.aln", delim = " ")
+#   cfile <- as.data.frame(map(cfile,function(x) gsub("\\s+", "",x)))
+#   colnames(cfile) <- c("AccNum", "Alignment")
+# }
---
+++ R/msa.R
@@ -0,0 +1,94 @@
+library(tidyverse)
+library(msa)
+# library(seqinr)
+library(Biostrings)
+library(tools)
+
+#output generated before hand:ideally y
+#is determined by sliders
+#for psp, don't need to get too complex, tree
+#and alignment generated backend
+#could find an output that is an image?
+#look at other outputs and functions of msa that produce other outputs
+#or other packages, to embed
+
+
+
+#'Multiple Sequence Alignment
+#'
+#'Generates a multiple sequence alignment from a fasta file
+#'
+#'msa_pdf is a function that reads a fasta file and generates a multiple sequence alignment as
+#'a pdf
+#'
+#'@param fasta_filepath Character. The path location of the fasta file to be read.
+#'@param lowerbound Numeric. The column that determines the starting location of the MSA.
+#'Default is NULL. If value is NULL, the entire multiple sequence alignment is printed.
+#'@param upperbound Numeric. The column that determines the ending location of the MSA.
+#'Default is NULL. If value is NULL, the entire multiple sequence alignment is printed.
+# #'@examples msa_pdf()
+msa_pdf <- function(fasta_filepath , lowerbound=NULL, upperbound=NULL){
+
+  #only works for a fasta file
+  #first convert tsv to fastafile: pretty easy but something needs to be done first
+  #tab replaced with something and < in front
+  # don't want accesation numbers, must be mapped to a species
+  #This is
+  my_seqs <- readAAStringSet(fasta_filepath)
+  my_seqs_msa <- msa(my_seqs)
+
+  #print the whole MSA if either bound is NULL
+  if(is.null(lowerbound) | is.null(upperbound)){
+    msaPrettyPrint(my_seqs_msa, output="tex",
+                   file=paste0(fasta_filepath, ".tex"),
+
+                   #aesthetic
+                   ##showNames="left", showLogo="top",
+                   showNames = "none",
+                   showLogo = "none",
+                   ##logoColors="rasmol", # “chemical”, “rasmol”, “hydropathy”, “structure”, “standard area”, “accessible area”
+                   shadingMode="functional", # or "similar"
+                   shadingModeArg="structure",
+                   shadingColors="blues",
+                   consensusColors="ColdHot",
+                   askForOverwrite=FALSE, verbose=FALSE,
+                   furtherCode=c("\\defconsensus{.}{lower}{upper}",
+                                 "\\showruler{1}{top}"))
+  }
+  #If bounds are not NULL
+  else{
+    msaPrettyPrint(my_seqs_msa, output="tex",
+                   file=paste0(fasta_filepath, ".tex"),
+
+                   #if user doesn't input either lowerbound or upperbound, default to either length, or 0
+                   #how do I get the length?
+                   y=c(lowerbound,upperbound),
+                   #aesthetic
+                   ##showNames="left", showLogo="top",
+                   showNames = "none",
+                   showLogo = "none",
+                   ##logoColors="rasmol", # “chemical”, “rasmol”, “hydropathy”, “structure”, “standard area”, “accessible area”
+                   shadingMode="functional", # or "similar"
+                   shadingModeArg="structure",
+                   shadingColors="blues",
+                   consensusColors="ColdHot",
+                   askForOverwrite=FALSE, verbose=FALSE,
+                   furtherCode=c("\\defconsensus{.}{lower}{upper}",
+                                 "\\showruler{1}{top}"))
+  }
+
+  texi2pdf(file = paste0(fasta_filepath,".tex"), clean= TRUE)
+}
+
+## Input files: Fasta format
+#my_seqs_file <- read_tsv("data/alignments/pspn-duf3046-aln/pspn.31seq.aln.txt")
+# colnames(my_seqs_file) <- c("name", "sequence")
+# my_alignment <- as(my_seqs_file, "BStringSet")
+
+#for functions, take in a file path? or maybe a "StringSet"
+#Parameters: filepath? Upperbound, Lowerbound, pdfortex?
+
+#fasta_filepath <- "C:/Users/samue/Desktop/pspn.31seq.aln.txt"
+#my_seqs <- readAAStringSet(fasta_filepath) #, format="fasta", seek.first.rec=T)
+#my_seqs_msa <- msa(my_seqs)
+
