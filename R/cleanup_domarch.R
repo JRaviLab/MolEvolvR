7c7f3a37f536eb1f489ca7b3e2ab54d89f208aac
Janani Ravi <janani@msu.edu>
2019-08-12 15:15:59 -0400

    minor fixes incl. var names
    Changed ...
    - input file path to data/acc_files instead of @samuelzornchen folder (Sam, one accessory file is missing. add a copy from your local folder to the repo subfolder, even if it's a place holder)
    - from eDNA_data to 'prot'
    - from '=' to '<-'
    - some indentation, spacing
    
    - Yet to change var names such as 'ge' and such...

--- R/cleanup_domarch.R
+++ R/cleanup_domarch.R
@@ -1,103 +1,106 @@
-##reading domains.rename
-#Can just use psp again
-#test run on SIg+TM+LADB... or DomArch
+## Functions to cleanup the domain architecture column
+
 library(tidyverse)
-domains_rename <- read_delim("C:/Users/samue/Desktop/domains.rename", "\t",
 
-                             escape_double = FALSE, col_names = FALSE,
-
+## Reading domains.rename
+# Can just use psp again
+# Test run on SIg+TM+LADB... or DomArch
+domains_rename <- read_delim("data/acc_files/domains.replace.txt",
+                             "\t", escape_double = FALSE, col_names = FALSE,
                              trim_ws = TRUE)
-#data must have 2 columns
-names(domains_rename)=c("old","new") #rename one column to old and the other to new
+# data must have 2 columns (old, new)
+names(domains_rename) <- c("old","new") #rename one column to old and the other to new
 
 domains_rename
 
-eDNA_data$archold=eDNA_data$arch
-#eDNA is cleanblastclust file, probably could test on DomArch
+## Reading input protein/domain file
+prot <- read_delim("data/rawdata_tsv/all.txt")
 
-#For loop iterates through the rows from 1 to the end of domains_rename$old
+prot$archold <- prot$DomArch
+#prot/eDNA_data is cleanblastclust file, probably could test on DomArch
+
+# For loop iterates through the rows from 1 to the end of domains_rename$old
 for (j in 1:length(domains_rename$old)) {
 
-  #eDNA_data$arch[grep(domains_rename$old[j], eDNA_data$arch)]=gsub(domains_rename$old[j], domains_rename$new[j],x = eDNA_data$arch[grep(domains_rename$old[j], eDNA_data$arch)])
-#regterm is the string "^domainofold\\+"
-  regterm=paste ("^",domains_rename$old[j],"\\+",sep="")
-#repterm is the string "domofnew+"
-  repterm=paste (domains_rename$new[j],"\\+",sep="")
-#All indeces in eDNA_data$arch that match regterm, are replaced with repterm
-  eDNA_data$arch[grep(regterm, eDNA_data$arch)]=gsub(regterm, repterm,x = eDNA_data$arch[grep(regterm, eDNA_data$arch)])
+  #prot$arch[grep(domains_rename$old[j], prot$arch)]=gsub(domains_rename$old[j], domains_rename$new[j],x = prot$arch[grep(domains_rename$old[j], prot$arch)])
+  #regterm is the string "^domainofold\\+"
+  regterm <- paste("^",domains_rename$old[j],
+                   "\\+",sep="")
+  #repterm is the string "domofnew+"
+  repterm <- paste(domains_rename$new[j],
+                   "\\+",sep="")
+  #All indeces in prot$arch that match regterm, are replaced with repterm
+  prot$arch[grep(regterm, prot$arch)] <- gsub(regterm, repterm,
+                                              x = prot$arch[grep(regterm, prot$arch)])
 
-#regterm now is "+domainofold$" (very similar to first regterm)
-  regterm=paste ("\\+",domains_rename$old[j],"$",sep="")
-#repterm is now "+domainofnew" (very similar to first repterm)
-  repterm=paste ("\\+",domains_rename$new[j],sep="")
-#All indeces in eDNA_data$arch that match second regterm are replaced with second repterm
-  eDNA_data$arch[grep(regterm, eDNA_data$arch)]=gsub(regterm, repterm,x = eDNA_data$arch[grep(regterm, eDNA_data$arch)])
+  #regterm now is "+domainofold$" (very similar to first regterm)
+  regterm <- paste("\\+",domains_rename$old[j],
+                   "$",sep="")
+  #repterm is now "+domainofnew" (very similar to first repterm)
+  repterm <- paste("\\+",domains_rename$new[j],
+                   sep="")
+  #All indeces in prot$arch that match second regterm are replaced with second repterm
+  prot$arch[grep(regterm, prot$arch)] <- gsub(regterm, repterm,
+                                              x = prot$arch[grep(regterm, prot$arch)])
 
-#regterm3 = "+domainofold+"
-  regterm=paste ("\\+",domains_rename$old[j],"\\+",sep="")
-#repterm3 = "+domainofnew+"
-  repterm=paste ("\\+",domains_rename$new[j],"\\+",sep="")
+  #regterm3 = "+domainofold+"
+  regterm <- paste("\\+", domains_rename$old[j],
+                   "\\+", sep="")
+  #repterm3 = "+domainofnew+"
+  repterm <- paste("\\+", domains_rename$new[j],
+                   "\\+", sep="")
 
   #repterm= domains_rename$new[j]
-#What does[1] do?
-#while iterates keeps going until no more strings in eDNA_data$arch match regterm
-  while ( length(eDNA_data$arch[grep(regterm[1], eDNA_data$arch)]) > 0) {
-#replace the regterm with repterm
-    eDNA_data$arch[grep(regterm, eDNA_data$arch)]=gsub(regterm, repterm,x = eDNA_data$arch[grep(regterm, eDNA_data$arch)])
-
+  #What does[1] do?
+  #while iterates keeps going until no more strings in prot$arch match regterm
+  while(length(prot$arch[grep(regterm[1], prot$arch)]) > 0) {
+    #replace the regterm with repterm
+    prot$arch[grep(regterm, prot$arch)] <- gsub(regterm, repterm,
+                                                x = prot$arch[grep(regterm, prot$arch)])
   }
-
 }
 
+## Reading domains.afterrename.group.tsv
 
-
-##reading domains.afterrename.group.tsv
-
-domains_afterrename_group <- read_delim("C:/Users/samue/Desktop/domains.afterrename.group.tsv",
-
+domains_afterrename_group <- read_delim("data/acc_files/domains.afterrename.group.tsv",
                                         "\t", escape_double = FALSE, col_names = FALSE,
-
                                         trim_ws = TRUE)
-
 domains_afterrename_group
 
 
-
-
-
-#generating domains to ignore and cleaning the names in arch
-#ge is a vector containing vectors of the strings in eDNA_data$arch that are split where there is a '\+'
-ge = unlist(strsplit(eDNA_data$arch, "\\+"))
+# Generating domains to ignore and cleaning the names in arch
+#ge is a vector containing vectors of the strings in prot$arch that are split where there is a '\+'
+ge <- unlist(strsplit(prot$arch, "\\+"))
 
 #domains.ignore = the elements in ge that aren't in domains_afterrename_group$X1
-domains.ignore=setdiff(ge, domains_afterrename_group$X1)
-#for loop essentially replaces the elements in eDNA_data$arch that match domains.ignore with
+domains.ignore <- setdiff(ge, domains_afterrename_group$X1)
+
+# for loop essentially replaces the elements in prot$arch that match domains.ignore with
 #\+, or maybe just delete them all
-#for loop goes from 1 to length of domains.ignore
+
+# for loop goes from 1 to length of domains.ignore
 for (j in 1:length(domains.ignore)) {
-#regterm = "\+domains.ignore\+", changes for each iteration
-  regterm=paste ("\\+",domains.ignore[j],"\\+",sep="")
-#while loop goes until no more terms in eDNA_data$arch match regterm
-  while ( length(eDNA_data$arch[grep(regterm[1], eDNA_data$arch)]) > 0) {
-#substitute "\+" for regterm in eDNA_data$arch
-   eDNA_data$arch[grep(regterm[1], eDNA_data$arch)]=gsub(regterm[1], "\\+",x = eDNA_data$arch[grep(regterm[1], eDNA_data$arch)] )
+  #regterm = "\+domains.ignore\+", changes for each iteration
+  regterm <- paste("\\+", domains.ignore[j], "\\+",
+                   sep="")
+  #while loop goes until no more terms in prot$arch match regterm
+  while(length(prot$arch[grep(regterm[1], prot$arch)]) > 0) {
+    #substitute "\+" for regterm in prot$arch
+    prot$arch[grep(regterm[1], prot$arch)] <- gsub(regterm[1], "\\+",
+                                                   x = prot$arch[grep(regterm[1], prot$arch)])
 
   }
-#regterm = "^domains.ignore\+|\+domains.ignore$"
-  regterm=paste ("^",domains.ignore[j],"\\+","|","\\+",domains.ignore[j],"$",sep="")
-#replace eDNA_data$arch elements that match regterm with "" (delete the elements that match domains.ignore)
-  eDNA_data$arch[grep(regterm[1], eDNA_data$arch)]=gsub(regterm[1], "",x = eDNA_data$arch[grep(regterm[1], eDNA_data$arch)] )
+  #regterm = "^domains.ignore\+|\+domains.ignore$"
+  regterm <- paste("^", domains.ignore[j], "\\+","|","\\+", domains.ignore[j],"$",
+                   sep="")
+  #replace prot$arch elements that match regterm with "" (delete the elements that match domains.ignore)
+  prot$arch[grep(regterm[1], prot$arch)] <- gsub(regterm[1], "",
+                                                 x = prot$arch[grep(regterm[1], prot$arch)] )
 
-
-
-  #eDNA_data$arch=gsub("\\+{2,}", "+", eDNA_data$arch)
-
-  #eDNA_data$arch=gsub("^\\+|\\+$", "", eDNA_data$arch)
-
+  #prot$arch=gsub("\\+{2,}", "+", prot$arch)
+  #prot$arch=gsub("^\\+|\\+$", "", prot$arch)
 }
 
 
-
-#cleaning up orphan SIG/TM archs
-
-eDNA_data=eDNA_data[- which (eDNA_data$arch == "SIG" | eDNA_data$arch == "TM"), ]
+## Cleaning up orphan SIG/TM archs
+prot <- prot[- which (prot$arch == "SIG" | prot$arch == "TM"), ]
