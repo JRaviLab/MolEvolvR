99fabf78fd291a4ca5c885f6ee5542364e81bdcc
Janani Ravi <janani@msu.edu>
2019-08-13 17:31:20 -0400

    add minor edits

--- R/clean_clust_file.R
+++ R/clean_clust_file.R
@@ -11,31 +11,41 @@ library(tidyverse)
 #' @param writepath A character designating where the tsv file of the cleaned cluster file will be written to. If value is NULL no
 #' file is written. Default NULL
 #' @param query A character identifying the query of the file.
-#' @examples clean_clust_file("data/1700.allfa.op_ins_cls",writepath=NULL,query="DUF1700")
+#' @examples clean_clust_file("data/pspa.op_ins_cls",writepath=NULL,query="pspa")
+
 clean_clust_file <- function(path, writepath=NULL,query){
   #Colnames for the cluster file
-  colnames_prot <- c("AccNum", "GenContext","PFAM", "DomArch","arch.TMSIG","Length" ,"GenName","Lineage","Species.old", "Annotation","GI")
+  colnames_prot <- c("AccNum", "GenContext", "PFAM", "DomArch", "arch.TMSIG",
+                     "Length", "GenName", "Lineage", "Species.old",
+                     "Annotation", "GI")
 
-  prot <- read_tsv(path,col_names = F)
+  prot <- read_tsv(path, col_names = F)
 
   #clust contains a column containing all the clustids
-  clust <- prot %>% filter(grepl("^#",X1))
+  clust <- prot %>% filter(grepl("^#", X1))
 
   #Separate all rows into columns by spaces and create ClustName and ClustID columns
   #First warning below
   prot <- prot %>%
-    separate(X1,colnames_prot,sep=("  +"))%>% mutate(ClustName = "",ClustID="")
-  #ind_with_num contains a list of the row numbers with # in them. This indicates that the row contains a clust id
-  ind_with_num <- which(grepl("^#",prot$AccNum))
+    separate(X1,colnames_prot,sep=("  +")) %>%
+    mutate(ClustName = "", ClustID="")
+  
+  #ind_with_num contains a list of the row numbers with # in them.
+  #This indicates that the row contains a clust id
+  ind_with_num <- which(grepl("^#", prot$AccNum))
+  
   #Separate the clustIDs (# 186;ClustName) by ";" into columns ClustID and ClusName
-  clsid <- separate(prot[ind_with_num,"AccNum"],col=AccNum, into=c("ClustID","ClusName"),sep = ";")
+  clsid <- separate(prot[ind_with_num, "AccNum"], col=AccNum,
+                    into=c("ClustID", "ClusName"), sep = ";")
+  
   #iterate throught the rows of clsid and get it into proper format
   #ex) # 186 -> 000001.186
   for(x in 1:length(clsid$ClustID)){
     lng <- str_length(x)
-    clsid$ClustID[x] <- gsub(pattern="# ", replacement = paste0(strrep("0",(6-lng)),x,"."),clsid$ClustID[x])
+    clsid$ClustID[x] <- gsub(pattern="# ",
+                             replacement = paste0(strrep("0",(6-lng)),x,"."),
+                             clsid$ClustID[x])
   }
-
 
   ind_with_num <- ind_with_num %>% append((length(prot$AccNum)+1))
   #Assign CLS id and CLS name to all rows
@@ -49,7 +59,9 @@ clean_clust_file <- function(path, writepath=NULL,query){
   #filter out the rows containing just the clustid and create a Query column
   prot <- filter(prot,!grepl("^#",prot$AccNum))%>% mutate(Query=query)
 
-  prot <- select(prot,AccNum,Query,ClustID,ClustName,GenContext,PFAM,DomArch,arch.TMSIG,Length,GenName,Lineage,Species.old,Annotation,GI)
+  prot <- select(prot, AccNum, Query, ClustID, ClustName, GenContext,
+                 PFAM, DomArch, arch.TMSIG, Length, GenName,
+                 Lineage, Species.old, Annotation, GI)
 
   write_tsv(prot, writepath)
 
