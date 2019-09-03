4cf3e8398ac0a421cf608462d32076f4c4f112f3
Janani Ravi <janani@msu.edu>
2019-09-03 17:34:48 -0400

    edit clustnames to keep/rm
    @samuelzornchen please take a look at the slight modifications made based on what we spoke last Friday.

--- R/identify_da.R
+++ R/identify_da.R
@@ -1,23 +1,40 @@
 ## Identifying domains and clusternames
-library(tidyverse)
-source("cleanup.R")
+library(tidyverse); library("wordcloud")
+source("R/cleanup.R")
 
 # Read master file
 prot <- read_delim("data/rawdata_tsv/all_raw.txt",
                    delim="\t", col_names=T, comment="#", trim_ws=T,
                    escape_double=FALSE, na="NA")
 
+# Read domains to keep
+domains_keep <- read_delim("data/acc_files/domains_keep.txt",
+                   delim="\t", col_names=T, comment="#", trim_ws=T,
+                   escape_double=FALSE, na="NA")
+
+# Identify unique domains to retain
+# Not used after Aug 2019
+# domains_keep <- prot %>%
+#   select(Query) %>%
+#   distinct() %>% arrange()
+
+# write_tsv(domains_keep, "data/acc_files/domains_keep.txt")
+
+
 # Identify unique cluster names
 clustnames <- prot %>%
   select(ClustName) %>%
-  distinct() %>% arrange() %>%
-  repeat2s(by_column="ClustName")
+  arrange() %>% #distinct() %>%
+  repeat2s(by_column="ClustName") %>%
+  count(ClustName, sort=T)
 
-write_tsv(clustnames, "data/acc_files/clustnames_uniq.txt")
+clustnames.keep <- clustnames %>%
+  filter(grepl(x=ClustName, pattern=paste(domains_keep$domains,
+                                          collapse="|")))
+clustnames.rm <- clustnames %>%
+  filter(!grepl(x=ClustName, pattern=paste(domains_keep$domains,
+                                          collapse="|")))
 
-# Identify unique domains to retain
-domains_keep <- prot %>%
-  select(Query) %>%
-  distinct() %>% arrange()
-
-write_tsv(domains_keep, "data/acc_files/domains_keep.txt")
+# Last written: Sep 03, 2019
+write_tsv(clustnames.keep, "data/acc_files/clustnames_keep.txt")
+write_tsv(clustnames.rm, "data/acc_files/clustnames_remove.txt")
