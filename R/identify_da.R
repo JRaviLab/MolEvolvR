7644ea09fffc5a4d0dbdedb26d6014127e8c9ff1
Janani Ravi <janani@msu.edu>
2019-09-03 14:11:34 -0400

    Update identify_da.R
    pushing some incomplete changes from last week.
    
    Co-Authored-By: Samuel Chen <samuelzornchen@users.noreply.github.com>

--- R/identify_da.R
+++ R/identify_da.R
@@ -2,21 +2,22 @@
 library(tidyverse)
 source("cleanup.R")
 
-
+# Read master file
 prot <- read_delim("data/rawdata_tsv/all_raw.txt",
                    delim="\t", col_names=T, comment="#", trim_ws=T,
                    escape_double=FALSE, na="NA")
 
-
 # Identify unique cluster names
-prot %>%
+clustnames <- prot %>%
   select(ClustName) %>%
   distinct() %>% arrange() %>%
-  repeat2s(by_column="ClustName") %>% # From ("cleanup.R")
-  write_tsv("data/acc_files/clustnames_uniq.txt")
+  repeat2s(by_column="ClustName")
+
+write_tsv(clustnames, "data/acc_files/clustnames_uniq.txt")
 
 # Identify unique domains to retain
-prot %>%
+domains_keep <- prot %>%
   select(Query) %>%
-  distinct() %>% arrange() %>%
-  write_tsv("data/acc_files/domains_keep.txt")
+  distinct() %>% arrange()
+
+write_tsv(domains_keep, "data/acc_files/domains_keep.txt")
