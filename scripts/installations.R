cbb64afb577acdc944703be8016cb5655890aae1
Janani Ravi <janani@msu.edu>
2019-10-17 09:48:41 -0400

    wip: fixing dependency/installation issues
    @samuelzornchen
    
    still not fixed.
    Idea: making things work on new users not primed for our package!

--- scripts/installations.R
+++ scripts/installations.R
@@ -1,16 +1,18 @@
-install.packages(c("tidyverse",
-    "here",
-    "UpSetR",
-    "gridExtra",
-    "roxygen2",
-    "docstring",
-    "igraph",
-    "ape",
-    "phylogram",
-    "phytools",
-    "msa",
-    "seqinr",
-    "ggtree",
-    "tidytree",
-    "Rcpp",
-    "tidytext"), dependencies=T)
+
+# CRAN installations
+install.packages(c("devtools", "BiocManager",
+                   "docstring", "roxygen2",
+                   "tidyverse", "here", "testthat", "reprex", "data.table",
+                   "rmarkdown", "knitr", "blogdown", "bookdown",
+                   "Rcpp", "rgl",
+                   "gridExtra", "cowplot", "gganimate", "plotly", "ggthemes",
+                   "UpSetR", "wordcloud2", "igraph", "tidytext",
+                   "ape", "phylogram", "msa", "seqinr", "phylotools",
+                   "tidytree", "ggtree"), dependencies=T)
+
+# Bioconductor installations
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install()
+
+# GitHub and Devtools installations
