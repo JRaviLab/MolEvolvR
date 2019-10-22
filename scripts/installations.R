1ed3bd391e7409e497204d573a2297b1e5547367
Janani Ravi <janani@msu.edu>
2019-10-22 10:36:46 -0400

    Update installations.R
    moved out data.table, msa and ggtree to separate installations & via bioconductor/devtools.
    
    OpenMP issues with clang errors persist.

--- scripts/installations.R
+++ scripts/installations.R
@@ -1,18 +1,24 @@
 
 # CRAN installations
-install.packages(c("devtools", "BiocManager",
-                   "conflicted","docstring", "roxygen2",
-                   "tidyverse", "here", "testthat", "reprex", "data.table",
+install.packages(c("devtools", "conflicted","docstring", "roxygen2",
+                   "tidyverse", "here", "testthat", "reprex",
                    "rmarkdown", "knitr", "blogdown", "bookdown",
                    "Rcpp", "rgl",
-                   "gridExtra", "cowplot", "gganimate", "plotly", "ggthemes",
+                   "gridExtra", "cowplot", "gganimate", "plotly",
+                   "ggthemes", "ggsci",
                    "UpSetR", "wordcloud2", "igraph", "tidytext",
-                   "ape", "phylogram", "msa", "seqinr", "phylotools",
-                   "tidytree", "ggtree"), dependencies=T)
+                   "ape", "phylogram", "seqinr", "phylotools", "phytools",
+                   "tidytree"), dependencies=T)
+
+install.packages("BiocManager", dependencies=T)
+# trouble with OpenMP
+# install.packages("data.table", dependencies=T)
 
 # Bioconductor installations
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install()
+BiocManager::install("msa")
+# BiocManager::install("ggtree") # ERROR w/ fopenmp
 
 # GitHub and Devtools installations
+devtools::install_github("GuangchuangYu/ggtree")
