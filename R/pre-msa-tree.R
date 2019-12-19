1b5e8de2a7d50da0010badcd2c41c138052e49ef
Janani Ravi <janani@msu.edu>
2019-12-19 18:31:51 -0500

    fix msa_pdf function
    @samuelzornchen
    
    fixed for now. demonstrated for pspa_snf7.
    - should run for all other proteins/domains.
    - should fix 'reduced' chunk because msaPrettyPrint doesn't seem to work for too many sequences. Many errors with msa/tex/pdf with 100+ seqs.
    - @samuelzornchen check if it runs on your Win system too.

--- R/pre-msa-tree.R
+++ R/pre-msa-tree.R
@@ -17,9 +17,8 @@ conflicted::conflict_prefer("filter", "dplyr")
 ## Pre-requistive functions ##
 ##############################
 ## Function to convert alignment 'aln' to fasta format for MSA + Tree
-convert_aln2fa <- function(input_file = "data/alignments/pspa_snf7.aln",
-                           lin = read_tsv("data/pspa_snf7.txt"),
-                           #lin_file = "data/pspa_snf7.txt",
+convert_aln2fa <- function(input_file = "data/rawdata_aln/pspa_snf7.gismo.aln",
+                           lin = read_tsv("data/rawdata_tsv/all_raw.txt"), #!! finally change to all_clean.txt!!
                            output_path = NULL,
                            reduced = FALSE) {
   #' Adding Leaves to an alignment file w/ accessions
@@ -50,7 +49,7 @@ convert_aln2fa <- function(input_file = "data/alignments/pspa_snf7.aln",
                     reduced=reduced)
   names <- aln$Leaf_Acc
   sequences <- aln$Alignment
-  aln <- data.table(names,sequences)
+  aln <- data.table(names, sequences)
 
   fasta <- ""
   for(i in 1:length(aln$names)){
@@ -108,7 +107,7 @@ add_leaves <- function(input_file = "data/rawdata_aln/pspa_snf7.gismo.aln",
   #' number to lineage mapping.
   #' Default is 'pspa.txt'
   #' @param reduced Boolean. If TRUE, a reduced data frame will be generated with
-  #' only one sequence per lineage
+  #' only one sequence per lineage. Default is FALSE.
   #' @examples add_leaves('pspa_snf7.aln', 'pspa.txt')
   #' @details The alignment file would need two columns: 1. accession +
   #' number and 2. alignment. The protein homolog accession to lineage mapping +
@@ -140,6 +139,8 @@ add_leaves <- function(input_file = "data/rawdata_aln/pspa_snf7.gismo.aln",
   #Removes rows with NA
   aln_lin <- aln_lin[complete.cases(aln_lin),]
 
+  ##!! REVISE or REMOVE??? MSA doesn't work with too many sequences!!
+  ##!! FIX ASAP.
   if(reduced){
     #Removes duplicate lineages
     aln_lin <- aln_lin %>% distinct(Lineage, .keep_all = TRUE)
@@ -159,18 +160,18 @@ add_leaves <- function(input_file = "data/rawdata_aln/pspa_snf7.gismo.aln",
                              str_sub(phylum, 1, 6)),
                       paste0(str_sub(genus, start=1, end=1),
                              str_sub(spp, start=1, end=3)),
-                      AccNum,
+                      # AccNum,
                       sep="_"))
   temp$leaf <- map(temp$leaf, to_titlecase)
   temp <- temp %>%
     mutate(Leaf_Acc = (paste(leaf,AccNum,sep="_")))
 
   #Combine and run through add leaves
-  #3 columns ACc Sequence Leaf resutl
+  #3 columns AccNum Sequence Leaf result
   #Create Leaf_AccNum pasted together
-  #2 columns Leaf)AccNum  and Sequence Far left
+  #2 columns Leaf_AccNum and Sequence Far left
   leaf_aln <- temp %>%
-    select(Leaf_Acc,Alignment)
+    select(Leaf_Acc, Alignment)
   return(leaf_aln)
 }
 
