60eef5a5d7a1fa46c7d481a90d7f64cd6c098fc5
Samuel Chen <chensam2@msu.edu>
2020-01-13 14:44:41 -0500

    Update dependencies.R

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -1,3 +1,5 @@
 install.packages(c("sourcetools", "geometry", "crosstalk", "vdiffr", "rnaturalearth", "DT", "devtools", "diffobj", "freetypeharfbuzz", "gdtools", "shiny", "testthat", "usethis"), dependencies=T)
 install.packages(c("shiny", "manipulateWidget", "quadprog", "fastmatch", "maps", "gtools", "mnormt", "covr", "testthat", "geiger", "phangorn", "DT", "rgl"), dependencies=T)
 install.packages(c("phytools", "phangorn"), dependencies=T)
+
+install.packages(c("Conflicted","here","tidyverse","pdftools","latexpdf","UpSetR","wordcloud"))
