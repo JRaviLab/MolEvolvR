cf27d0ef14e7beae0a3decdf163e13e63840287c
Samuel Chen <chensam2@msu.edu>
2020-02-27 13:41:17 -0500

    Update Network filtering

--- R/GC_network_directed.R
+++ R/GC_network_directed.R
@@ -9,8 +9,8 @@ conflicted::conflict_prefer("filter", "dplyr")
 #### Network FUNCTIONS ####
 ###########################
 gc_directed_network <- function(prot, column = "GenContext",
-                       cutoff_type = "Lineage", cutoff = 40,
-                       layout = "grid"){
+                                cutoff_type = "Lineage", cutoff = 40,
+                                layout = "grid"){
   #'Genomic Context Directed Network
   #'
   #'This function creates a Genomic Context network from the 'GenContext' column.
@@ -26,24 +26,44 @@ gc_directed_network <- function(prot, column = "GenContext",
   #'Only use domains that appear in cutoff or greater lineages if cutoff_type is Lineage.
   #'@param layout Character. Layout type to be used for the network. Options are:
   #'\itemize{\item "grid" \item "circle" \item "random" \item "auto"}
-  #'@examples gc_directed_network(pspa, cutoff_type = "Total Count", cutoff = 55)
-
-  # by domain networks or all, as required.
-  # ye is either all of prot.list or centered on one domain
+  #'@examples gc_directed_network(pspa, column = "GenContex", cutoff = 55)
 
   column_name <- sym(column)
-  if(cutoff_type == "Lineage"){
-    # Cutoff is determined by how many different lineages the GC appears in
-    lin_summary <- prot %>% summ.GC.byLin() %>% group_by(GenContext) %>% summarize(totallin = n())
-    GC_above_cutoff <- (lin_summary %>% filter(totallin >= cutoff))[[column]]
-  }
-  else if(cutoff_type == "Total Count"){
-    # Cutoff is determined by the overall amount of times a GC appears
-    GC_above_cutoff <- (prot %>% total_counts( column =  column, cutoff = cutoff))[[column]]
+
+
+  # Identify cutoff
+  ### Use GC counts from all of prot for node weights
+  GC_count <- elements2words(prot,column = {{column}},conversion_type = "gc2da") %>% words2wc()
+
+  # Get Cummulative Percentage of Each GC element
+  TotalWordsFreq  = sum(GC_count$freq)
+  GC_count <- GC_count %>% mutate("WordsPercentage" = 0) %>% arrange(freq)
+
+  total_counter = 0
+  for(x in 1:length(GC_count$freq)){
+    total_counter = total_counter + GC_count$freq[x]
+    GC_count$WordsPercentage[x] = total_counter/TotalWordsFreq * 100
   }
 
-  prot <- prot[which(prot[[{{column_name}}]] %in% GC_above_cutoff),]
+  GC_above <- GC_count %>% filter(WordsPercentage >= 100 - cutoff)
+  if(length(GC_above$words) == 0){
+    cutoff_count = 0
+  }
+  else{
+    cutoff_count = GC_above$freq[1]
+  }
 
+  # node weights contains the occurances of all domains above the count
+  node_weights <- (GC_count %>% filter(freq >= cutoff_count) %>% select(-WordsPercentage))
+  ####
+
+
+  GC_above <- paste(node_weights$words, collapse = "$|^")
+  GC_above <- paste0("^",GC_above, "$")
+
+
+  #### rev_gc(gc_row)
+  #### Reverse the order of the split up GC elements whenever a change in transcription direction is found?
   rev_gc <- function(x){
     direction <- 0
     op_dir <- "<-|->"
@@ -81,54 +101,98 @@ gc_directed_network <- function(prot, column = "GenContext",
 
   gencontext <- prot$GenContext
 
+  # Prime for splitting
   gencontext <- gsub(pattern = ">",replacement = ">|",x = gencontext)
-
   gencontext <- gsub(pattern = "<",replacement = "|<",x = gencontext)
 
-
+  # Split GC
   gen_split <- gencontext %>% strsplit("\\|")
 
+  # Remove non edges
   if(any(lengths(gen_split)==1)) {
     gen_split=gen_split[-which(lengths(gen_split)==1)]
   }
 
+  # Apply rev_gc to all rows
   gen_split_reoriented <- map(.x = gen_split, .f = rev_gc)
 
-
+  # Remove all arrows
   gen_split_reoriented <- map2(.x = gen_split_reoriented,.y = "<-|->", .f = str_remove_all)
-
 
   te <- unlist(lapply(gen_split_reoriented, function(x) sapply(1:(length(x)-1), function(y) x[y]))) #list elements 1 through n-1
   ye <- unlist(lapply(gen_split_reoriented, function(x) sapply(1:(length(x)-1), function(y) x[y+1]))) #list elements 2 through n
+
+  # Edge list
   pwise <- cbind(te,ye)
-  if(any(pwise==""|pwise=="?")) {
-    pwise=pwise[-which((pwise[,1]=="") | (pwise[,1]=="?") | (pwise[,2]=="")| (pwise[,2]=="?")),]
+
+  # Remove rows that are not in cutoff or have bad characters
+  if(any(pwise==""|pwise=="?"| !grepl(GC_above,pwise))) {
+    pwise=pwise[-which((pwise[,1]=="") | (pwise[,1]=="?") | (pwise[,2]=="")| (pwise[,2]=="?")
+                       | !grepl(GC_above, pwise[,1]) | !grepl(GC_above, pwise[,2]) ),]
   }
 
 
 
-  pwise.ass <- sapply(1:length(pwise[,1]), function(x) paste(pwise[x,],collapse = "->"))
-  e.sz <- sort(table(pwise.ass),decreasing = T)
-  v.sz <- sort(table(pwise),decreasing = T)
-  pwise <- strsplit(names(e.sz), "\\->")
-  pwise <- cbind(unlist(lapply(pwise, function(x) x[1])),unlist(lapply(pwise, function(x) x[2])))
+  # First create list graph from edge list, then add the
+  # vertices that are above the cutoff but not in the graph
 
-  # Plotting the network
-  g <- graph_from_edgelist(pwise, directed=TRUE)
-  # scaling vertex size
-  V(g)$size <- v.sz[V(g)$name]
-  V(g)$size <- (V(g)$size-min(V(g)$size))/(max(V(g)$size)-min(V(g)$size))*20+10 # scaled by degree
-  # setting vertex color by size
-  V(g)$color <- rainbow(5,alpha = .5)[round((V(g)$size-min(V(g)$size))/(max(V(g)$size)-min(V(g)$size))*4+1)]
-  V(g)$frame.color <- V(g)$color
-  # scaling edge width
-  E(g)$width <- e.sz
-  E(g)$width <- ifelse(log(E(g)$width)==0, .3,log(E(g)$width))
-  # coloring edges by width
-  ew <- c(2.7,4.5)
-  E(g)$color <- sapply(E(g)$width,
-                       function(x) if(x>=ew[1] && x<=ew[2]) E(g)$color="cadetblue" else if(x>ew[2]) E(g)$color="maroon" else E(g)$color="gray55")
+  if(length(pwise) != 0){
+    # Original
+    pwise.ass <- sapply(1:length(pwise[,1]), function(x) paste(pwise[x,],collapse = "->"))
+    e.sz <- sort(table(pwise.ass),decreasing = T)   # Counts of each edge (edge weight)
+    v.sz <- sort(table(pwise),decreasing = T)       # Counts of each vertex in the edges -- May be obsolete if i just r
+    # Relace with al the node weights
+    pwise <- strsplit(names(e.sz), "\\->")
+    pwise <- cbind(unlist(lapply(pwise, function(x) x[1])),unlist(lapply(pwise, function(x) x[2])))
 
+    g <- graph_from_edgelist(pwise, directed=TRUE)
+
+    ### Handle Edge Weights and Color
+    # scaling edge width
+    E(g)$width <- e.sz
+    E(g)$width <- ifelse(log(E(g)$width)==0, .3,log(E(g)$width))
+    # coloring edges by width
+    ew <- c(2.7,4.5)
+    E(g)$color <- sapply(E(g)$width,
+                         function(x) if(x>=ew[1] && x<=ew[2]) E(g)$color="cadetblue" else if(x>ew[2]) E(g)$color="maroon" else E(g)$color="gray55")
+  }
+  else{
+    g <- make_empty_graph()
+  }
+
+  # Go through all vertices above cutoff, add them to graph if they don't exist
+  # Assign them their respective weights with V(g)$size <- v.sz[V(g)$name]
+
+  node_weights <- pivot_wider(node_weights, names_from = words, values_from = freq)
+
+  # vertices to be added -- No edges in graph
+  v_to_add <- colnames(node_weights)[which(!(colnames(node_weights) %in% V(g)$name))]
+
+  # Add isolated vertices
+  g <- g + vertices(v_to_add)
+
+  # Assign Weights to each vertex
+  V(g)$size <- as.integer(node_weights[V(g)$name])
+
+  # if only one node, set to fixed size Or if min and max are same number
+  if(length(V(g)$size) == 1 | min(V(g)$size) == max(V(g)$size)  )
+  {
+    V(g)$size <- 25
+    V(g)$color <- rainbow(1, alpha = .5)
+    V(g)$frame.color <- V(g)$color
+  }
+
+  # Resize based on difference between sizes of max and size of min
+  # (length(V(g)$size) >1)
+  else{
+    ## Below scaling will not work if only one vertex
+    V(g)$size <- (V(g)$size-min( V(g)$size) )/(max(V(g)$size)-min(V(g)$size))*20+10 # scaled by degree
+
+    v_size <-(V(g)$size)
+    # setting vertex color by size
+    V(g)$color <- rainbow(5,alpha = .5)[round( (v_size-min(v_size))/(max(v_size)-min(v_size)*4+1))]
+    V(g)$frame.color <- V(g)$color
+  }
 
   switch(layout,
          "random" = plot.igraph(g,layout = layout_randomly(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black"),
--- R/network.R
+++ R/network.R
@@ -13,7 +13,7 @@ conflicted::conflict_prefer("filter", "dplyr")
 ###########################
 #### Network FUNCTIONS ####
 ###########################
-domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff_type = "Lineage", cutoff = 1, layout = "grid"){
+domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff = 1, layout = "grid"){
   #'Domain Network
   #'
   #'This function creates a domain network from the 'DomArch' column.
@@ -22,11 +22,7 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
   #'
   #'@param prot A data frame that contains the column 'DomArch'.
   #'@param column Name of column containing Domain architecture from which nodes and edges are generated.
-  #'@param cutoff_type Character. Used to determine how data should be filtered. Either
-  #'\itemize{\item "Lineage" to filter domains based off how many lineages the Domain architecture appears in
-  #'\item "Total Count" to filter off the total amount of times a domain architecture occurs }
-  #'@param cutoff Integer. Only use domains that occur at or above the cutoff for total counts if cutoff_type is "Total Count".
-  #'Only use domains that appear in cutoff or greater lineages if cutoff_type is Lineage.
+  #'@param cutoff Integer. Only use domains that occur at or above the cutoff
   #'@param layout Character. Layout type to be used for the network. Options are:
   #'\itemize{\item "grid" \item "circle" \item "random" \item "auto"}
   #'@examples domain_network(pspa)
@@ -35,18 +31,7 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
   # ye is either all of prot.list or centered on one domain
 
   column_name <- sym(column)
-  if(cutoff_type == "Lineage"){
-    lin_summary <- prot %>% summ.DA.byLin() %>% summ.DA()
-    doms_above_cutoff <- (lin_summary %>% filter(totallin >= cutoff))[[column]]
-  }
-  else if(cutoff_type == "Total Count"){ #Change this type?
-    doms_above_cutoff <- (prot %>% total_counts( column =  column, cutoff = cutoff))[[column]]
-  }
 
-  prot <- prot[which(prot[[as_string(column_name)]] %in% doms_above_cutoff),]
-
-  #prot.list=total$arch
-  #prot.list <- unlist(lapply(prot.list, function(x) gsub(x,pattern = "\\?",replacement = "X")))
   prot$DomArch.ntwrk <- as_vector(prot %>% select({{column}})) %>% # testing with prot$DomArch.orig
     str_replace_all(coll(pattern="\\?", ignore_case=T), "X")
 
@@ -66,34 +51,114 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
   domain.list <- ye#strsplit(ye,"\\+") #[[3]] or whatever index of the list(ye) contains domntwk
   if(any(lengths(domain.list)==1)) {
     domain.list=domain.list[-which(lengths(domain.list)==1)]
-    }
+  }
   te <- unlist(lapply(domain.list, function(x) sapply(1:(length(x)-1), function(y) x[y]))) #list elements 1 through n-1
   ye <- unlist(lapply(domain.list, function(x) sapply(1:(length(x)-1), function(y) x[y+1]))) #list elements 2 through n
   pwise <- cbind(te,ye)
-  if(any(pwise=="")) {
-    pwise=pwise[-which((pwise[,1]=="") | pwise[,2]==""),]
-    }
-  pwise.ass <- sapply(1:length(pwise[,1]), function(x) paste(pwise[x,],collapse = "->"))
-  e.sz <- sort(table(pwise.ass),decreasing = T)  # Edge weights
-  v.sz <- sort(table(pwise),decreasing = T)   # Vertex weights
-  pwise <- strsplit(names(e.sz), "\\->")
-  pwise <- cbind(unlist(lapply(pwise, function(x) x[1])),unlist(lapply(pwise, function(x) x[2])))
 
-  # Plotting the network
-  g <- graph_from_edgelist(pwise, directed=TRUE)
-  # scaling vertex size
-  V(g)$size <- v.sz[V(g)$name]
-  V(g)$size <- (V(g)$size-min(V(g)$size))/(max(V(g)$size)-min(V(g)$size))*20+10 # scaled by degree
-  # setting vertex color by size
-  V(g)$color <- rainbow(5,alpha = .5)[round((V(g)$size-min(V(g)$size))/(max(V(g)$size)-min(V(g)$size))*4+1)]
-  V(g)$frame.color <- V(g)$color
-  # scaling edge width
-  E(g)$width <- e.sz
-  E(g)$width <- ifelse(log(E(g)$width)==0, .3,log(E(g)$width))
-  # coloring edges by width
-  ew <- c(2.7,4.5)
-  E(g)$color <- sapply(E(g)$width,
-                    function(x) if(x>=ew[1] && x<=ew[2]) E(g)$color="cadetblue" else if(x>ew[2]) E(g)$color="maroon" else E(g)$color="gray55")
+  ### Use dom counts from all of prot for node weights
+  dom_count <- elements2words(prot,column = {{column}},conversion_type = "da2doms") %>% words2wc()
+
+  ### Use counts from domlist (That filter out the singles) for the edgeweights
+  ## Get wordcount for domains (for filtering purposes)
+  #dl <- data.frame("Doms" = unlist(domain.list), stringsAsFactors = F)
+  #dom_count <- elements2words(dl,column = "Doms",conversion_type = "da2doms") %>% words2wc()
+
+
+  TotalWordsFreq  = sum(dom_count$freq)
+  dom_count <- dom_count %>% mutate("WordsPercentage" = 0) %>% arrange(freq)
+
+  total_counter = 0
+  for(x in 1:length(dom_count$freq)){
+    total_counter = total_counter + dom_count$freq[x]
+    dom_count$WordsPercentage[x] = total_counter/TotalWordsFreq * 100
+  }
+
+  doms_above <- dom_count %>% filter(WordsPercentage >= 100 - cutoff)
+  if(length(doms_above$words) == 0){
+    cutoff_count = 0
+  }
+  else{
+    cutoff_count = doms_above$freq[1]
+  }
+
+  # node weights contains the occurances of all domains above the count
+  node_weights <- (dom_count %>% filter(freq >= cutoff_count) %>% select(-WordsPercentage))
+  ####
+
+
+  doms_above <- paste(node_weights$words, collapse = "$|^")
+  doms_above <- paste0("^",doms_above, "$")
+
+  # Filter out the ones rows containing edges that contain domains below cutoff
+  if(any(pwise==""|!grepl(doms_above,pwise))) {
+    pwise=pwise[-which((pwise[,1]=="") | pwise[,2]==""
+                       | !grepl(doms_above, pwise[,1]) | !grepl(doms_above, pwise[,2]) ),]
+  }
+
+
+  # First create list graph from edge list, then add the
+  # vertices that are above the cutoff but not in the graph
+
+  if(length(pwise) != 0){
+    # Original
+    pwise.ass <- sapply(1:length(pwise[,1]), function(x) paste(pwise[x,],collapse = "->"))
+    e.sz <- sort(table(pwise.ass),decreasing = T)   # Counts of each edge (edge weight)
+    v.sz <- sort(table(pwise),decreasing = T)       # Counts of each vertex in the edges -- May be obsolete if i just r
+    # Relace with al the node weights
+    pwise <- strsplit(names(e.sz), "\\->")
+    pwise <- cbind(unlist(lapply(pwise, function(x) x[1])),unlist(lapply(pwise, function(x) x[2])))
+
+    g <- graph_from_edgelist(pwise, directed=TRUE)
+
+    ### Handle Edge Weights and Color
+    # scaling edge width
+    E(g)$width <- e.sz
+    E(g)$width <- ifelse(log(E(g)$width)==0, .3,log(E(g)$width))
+    # coloring edges by width
+    ew <- c(2.7,4.5)
+    E(g)$color <- sapply(E(g)$width,
+                         function(x) if(x>=ew[1] && x<=ew[2]) E(g)$color="cadetblue" else if(x>ew[2]) E(g)$color="maroon" else E(g)$color="gray55")
+  }
+  else{
+    g <- make_empty_graph()
+  }
+
+  # Go through all vertices above cutoff, add them to graph if they don't exist
+  # Assign them their respective weights with V(g)$size <- v.sz[V(g)$name]
+
+  node_weights <- pivot_wider(node_weights, names_from = words, values_from = freq)
+
+  # vertices to be added -- No edges in graph
+  v_to_add <- colnames(node_weights)[which(!(colnames(node_weights) %in% V(g)$name))]
+
+  # Add isolated vertices
+  g <- g + vertices(v_to_add)
+
+  # Assign Weights to each vertex
+  V(g)$size <- as.integer(node_weights[V(g)$name])
+
+  # if only one node, set to fixed size Or if min and max are same number
+  if(length(V(g)$size) == 1 | min(V(g)$size) == max(V(g)$size)  )
+  {
+    V(g)$size <- 25
+    V(g)$color <- rainbow(1, alpha = .5)
+    V(g)$frame.color <- V(g)$color
+  }
+
+  # Resize based on difference between sizes of max and size of min
+  # (length(V(g)$size) >1)
+  else{
+    ## Below scaling will not work if only one vertex
+    V(g)$size <- (V(g)$size-min( V(g)$size) )/(max(V(g)$size)-min(V(g)$size))*20+10 # scaled by degree
+
+    v_size <-(V(g)$size)
+    # setting vertex color by size
+    V(g)$color <- rainbow(5,alpha = .5)[round( (v_size-min(v_size))/(max(v_size)-min(v_size)*4+1))]
+    V(g)$frame.color <- V(g)$color
+  }
+
+
 
 
   switch(layout,
@@ -101,7 +166,7 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
          "grid" = plot.igraph(g,layout = layout_on_grid(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black"),
          "circle" = plot.igraph(g,layout = layout.circle(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black"),
          "auto" =plot.igraph(g,layout = layout.auto(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black")
-         )
+  )
 
   #plot.igraph(g,layout = layout_randomly(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black")
   ### Tkplot does not work with shiny
@@ -109,9 +174,3 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
   #       vertex.label.dist=1,asp=0,
   #       edge.curved=F, vertex.label.color="black") #simple
 }
-
-
-
-#pspa_grouped <- pspa %>% group_by(DomArch) %>% summarise(n())
-#Counts of all the relations: bind back into df and then can do filtering based off it?
-#Else can i
