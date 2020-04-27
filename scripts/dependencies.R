52a9fe4f0d1dc190a4843e2cf28bf8486bf54baf
Janani Ravi <janani@msu.edu>
2020-04-27 15:10:47 -0400

    edit dep installations

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -1,7 +1,9 @@
 install.packages(c("sourcetools", "geometry", "crosstalk", "vdiffr", "rnaturalearth", "DT", "devtools", "diffobj", "freetypeharfbuzz", "gdtools", "shiny", "testthat", "usethis"), dependencies=T)
-install.packages(c("shiny", "manipulateWidget", "quadprog", "fastmatch", "maps", "gtools", "mnormt", "covr", "testthat", "geiger", "phangorn", "DT", "rgl"), dependencies=T)
+install.packages(c("manipulateWidget", "quadprog", "fastmatch", "maps", "gtools", "mnormt", "covr", "geiger", "rgl"), dependencies=T)
 install.packages(c("phytools", "phangorn"), dependencies=T)
 
-install.packages(c("Conflicted","here","tidyverse","pdftools","latexpdf","UpSetR","wordcloud"))
+## Other packages
+install.packages(c("conflicted","here","tidyverse","pdftools","latexpdf","UpSetR","wordcloud"), dependencies=T)
+install.packages(c("gggenes", "ggraph", "igraph", "ggdendro", "gganimate", "ggnetwork", "networkD3", "geomnet", "ggrepel", "cowplot", "ggtree"), dependencies=T)
 
-remotes::install_github("mhahsler/rMSA")
+remotes::install_github("mhahsler/rMSA", force=T)
