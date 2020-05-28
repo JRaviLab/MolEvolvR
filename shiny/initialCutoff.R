4816e476b3e3d780008ae088627722c1ed473f82
Samuel Chen <chensam2@msu.edu>
2020-05-28 12:35:59 -0400

    Update app

--- shiny/initialCutoff.R
+++ shiny/initialCutoff.R
@@ -3,27 +3,31 @@ library(tidyverse)
 
 
 
-top_n_cutoff <- function(prot, column, n_rows){
-  # column <- sym(column)
-
-  # Summarize by lineage and get total counts
-  prot_count <- total_counts(prot, column, cutoff = 100) %>%
-    filter(count != 1) # Remove singles
-
-  row_len <- nrow(prot_count)   # Number of rows corresponds to the amount of tiles
-  # Restrict by columns of heatmap and rows of heatmap separately?
-
-  if(row_len <= n_rows){
-    # Number of rows is less than the max rows
-    cutoff_perc <- floor(prot_count$CummulativePercent[row_len])
-  }
-  else{
-    # Number of rows exceeds limit and cutoff must be established
-    cutoff_perc <- floor(prot_count$CummulativePercent[n_rows])
-  }
-
-  return(cutoff_perc)
-}
+# top_n_cutoff <- function(prot, column, n_rows, RemoveAstrk = FALSE){
+#   # column <- sym(column)
+#   if(RemoveAstrk)
+#   {
+#     prot <- prot %>% remove_astrk(colname = column)
+#   }
+#
+#   # Summarize by lineage and get total counts
+#   prot_count <- total_counts(prot, column, cutoff = 100) %>%
+#     filter(count != 1) # Remove singles
+#
+#   row_len <- nrow(prot_count)   # Number of rows corresponds to the amount of tiles
+#   # Restrict by columns of heatmap and rows of heatmap separately?
+#
+#   if(row_len <= n_rows){
+#     # Number of rows is less than the max rows
+#     cutoff_perc <- floor(prot_count$CummulativePercent[row_len])
+#   }
+#   else{
+#     # Number of rows exceeds limit and cutoff must be established
+#     cutoff_perc <- floor(prot_count$CummulativePercent[n_rows])
+#   }
+#
+#   return(cutoff_perc)
+# }
 
 top_n_rows_cutoff <- function(prot, column, n_rows){
   # Obtain counts of all DomArchs/GC/Lineages
@@ -45,13 +49,15 @@ top_n_rows_cutoff <- function(prot, column, n_rows){
   # Rows is the total number of unique GCs/DAs/Lineages
   row_len <- nrow(prot_counts)
 
-  print(paste0("Row Length: ", row_len))
+  # print(paste0("Row Length: ", row_len))
   # Cumulative percentages
   total_counter = 0
   for(i in row_len:1){
     total_counter = total_counter + prot_counts$count[i]
     prot_counts$CummPercentage[i] = total_counter/total_count * 100
   }
+
+  prot_counts <- prot_counts %>% filter(!grepl("^-$", {{column}}))
 
   if(row_len < n_rows){
     # Less than limit, can include all data
@@ -61,15 +67,100 @@ top_n_rows_cutoff <- function(prot, column, n_rows){
     # Exceeds row limit
     cutoff_perc <- 100 -prot_counts$CummPercentage[n_rows]
   }
-
-
   return(cutoff_perc)
 
 }
 
+RowNums <- function(prot, column){
+  column <- sym(column)
+  prot_counts <- prot %>%
+    group_by({{column}}) %>%
+    summarise(count = n()) %>%
+    #filter(count != 1) %>%  # Filter out singles
+    arrange(desc(count))
+
+  # Rows is the total number of unique GCs/DAs/Lineages
+  row_len <- nrow(prot_counts)
+  return(row_len)
+}
 
 
 
+max_word_percents <- function(data, colname)
+{
+  # Get The max rows (words) of the upset plots
+  # Get the max percent for each word
+
+  # Get Total Counts
+  # colname = string(colname)
+  tc <- data %>% total_counts(column =  colname, cutoff = 100, RowsCutoff = F, digits = 5)
+
+  column <- sym(colname)
+
+  if(grepl("GenContext", colname, ignore.case = T))
+  {
+    type = "gc2da"
+  }
+  else if(grepl("DomArch|ClustName", colname, ignore.case = T))
+  {
+    type = "da2doms"
+  }
+
+  elements_Percs = tc %>% select({{column}}, CumulativePercent) %>% distinct()
+  # Get words from filter
+  words.tc <- elements_Percs %>% select({{column}}) %>% elements2words(column = colname,conversion_type = type)
+  # names(words.tc)[1] <- "words"
+  words.tc <- words.tc %>% str_split(pattern = " ") %>% unlist()
+  words.tc <- words.tc[2:(length(words.tc)-1)]
+
+  max_perc_words = data.frame("Words" = words.tc, "MaxPercent" = double(length(words.tc)), stringsAsFactors = F)
+  max_perc_words = max_perc_words %>% distinct()
+
+  listwords = max_perc_words$Words
+
+  listwords = str_replace_all(listwords, "\\(", "\\\\(") %>%
+    str_replace_all("\\)", "\\\\)") %>%
+    str_replace_all("\\+", "\\\\+")
+  for(i in 1:nrow(max_perc_words))
+  {
+    # Iterate over all words and find their max percent
+    cur_word = listwords[i]
+    # Filter by the word
+    word_percs = elements_Percs %>% filter(grepl(pattern = cur_word,x = {{column}}))
+
+    max_perc = max(word_percs$CumulativePercent)
+
+    max_perc_words$MaxPercent[i] = max_perc
+  }
+
+  max_perc_words = max_perc_words %>% arrange(-MaxPercent)
+  return(max_perc_words)
+}
+
+
+
+# GetLineageCounts <- function(prot,lineage_col = "Lineage")
+# {
+#   # Get the row numbers for all by using the total counts
+#   # Also get the initial count percentage,
+#   lineage_col <- sym(lineage_col)
+#
+#   all_lins <- prot %>% group_by({{lineage_col}}) %>% summarise(count = n()) %>% arrange(-count)
+#
+#
+#   sum_count <- sum(all_lins$count)
+#   all_lins <- all_lins %>% mutate("IndividualPercent" = count/sum_count*100) %>%
+#     mutate("CumulativePercent" = 0)
+#
+#   total_counter = 0
+#   for(x in nrow(all_lins):1){
+#     total_counter = total_counter + all_lins$count[x]
+#     all_lins$CumulativePercent[x] = total_counter/sum_count * 100
+#   }
+#
+#   return(all_lins)
+#
+# }
 
 # prot_da_lin <- reactive({switch(input$linSelec,
 #                                 "PspA"= pspa_DA_Lin,
--- shiny/shinyfunctions.R
+++ shiny/shinyfunctions.R
@@ -3,53 +3,12 @@ library(UpSetR)
 library(gridExtra)
 library(docstring)
 
-
-#input a table of type xxx_GC_Lin for lineagecounts
-#input a cutoff value for cutoff. A table with all total counts of values greater than cutoff will be generated
-
-# replace_doms_all <- function(prot,domains_rename, domains_remove){
-#   DomArch.old <- prot$DomArch
-#
-#   #replace domains based on the domains_rename list
-#   for(j in 1:length(domains_rename$old)){
-#     prot <- map(prot,function(x) x %>% str_replace_all(as.vector(domains_rename$old[j]),as.vector(domains_rename$new[j])))
-#   }
-#   # #remove domains based on the domains_remove list
-#   for(j in 1:length(as.vector(domains_remove$domains))){
-#     prot <- map(prot,function(x)x %>%str_remove_all(as.vector(domains_remove$domains[j])))
-#   }
-#   #remove '+' at the start and end, as well as consecuative '+'
-#   prot <- map(prot, function(x) x%>%  str_replace_all("\\++\\+","\\+")
-#               %>% str_replace_all("^\\+","") %>%
-#                 str_replace_all("\\+$",""))
-#
-#                        # str_replace_all("\\+", " ") %>%
-#                        # str_replace_all("(?i)\\b([a-z0-9_-]+)\\b(?:\\s+\\1\\b)+", "\\1(s)") %>%
-#                        # str_replace_all(" ", "+")) %>% as.data.frame()
-#
-#   return(cbind(as.data.frame(prot),DomArch.old))
-# }
-
-#rd <- replace_doms_all(all_op_ins,domains.replace,domains.remove)
-
-#Replace twos for all dataframe
-# ab<-map(all_op_ins,function(x) x %>%  		str_replace_all("\\+", " ") %>%
-#          str_replace_all("(?i)\\b([a-z0-9_-]+)\\b(?:\\s+\\1\\b)+", "\\1(s)") %>%
-#          str_replace_all(" ", "+")) %>% as.data.frame()
-
-#repeats2s <- function(x){
-  # 	x %>%
-  		# str_replace_all("\\+", " ") %>%
-  		# str_replace_all("(?i)\\b([a-z0-9_-]+)\\b(?:\\s+\\1\\b)+", "\\1(s)") %>%
-  		# str_replace_all(" ", "+")
-  # }
-
 heatmap_slider <- function(prot_lin,type){
   protcounts <- prot_lin %>% group_by(
     switch(type,
            da=DomArch.norep,
            gc=GenContext.norep
-           )) %>%
+    )) %>%
     summarise(totalcount=sum(count)) %>% arrange(desc(totalcount))
   rn <- length(protcounts$totalcount)
   if(rn<=10){
@@ -66,99 +25,79 @@ heatmap_slider <- function(prot_lin,type){
 }
 
 
-#very similar to upset.plot but with the wc param. This param is used for the word count file
-#Word Count file is using words2wc as of now
-lineage.upset <- function(query_data,cutoff,type, wc){
+rownumber_to_cutoff <- function(query_data, rownumber, col, RemoveAstrk = F)
+{
+  # Convert Rownumber input counts to percentage cutoffs that plotting functions require
+  #
 
-  switch(type,
-         da2doms=colname <- "DomArch",
-         gc2da= colname <- "GenContext")
+  # Row number of heatmap corresponds to number of GC/ DA
 
-  words.gecutoff <- filter(wc, freq>=cutoff)
-
-  ## Create columns for domains/DAs and fill them with 1/0
-  for(i in words.gecutoff$words){
-    j <-str_replace_all(string=i, pattern="\\(",replacement="\\\\(")
-    j <- str_replace_all(string=j, pattern="\\)", replacement="\\\\)")
-    j <- str_replace_all(string=j, pattern="\\+", replacement="\\\\+")
-    j <- str_replace_all(string=j, pattern="\\_",replacement="\\\\_")
-    query_data[[i]]<-if_else(grepl(j,as.matrix(query_data[,colname])),
-                              true=1, false=0)
+  if(RemoveAstrk)
+  {
+    query_data <- query_data %>% remove_astrk(colname = col)
   }
 
-  ## Creating UpSet data
-  upset <- query_data %>%
-    select(AccNum, Lineage, GenContext, DomArch,words.gecutoff$words) %>%
-    mutate_all(list( ~ if(is.numeric(.))as.integer(.) else .))  %>%
-    as.data.frame()
+  column <- sym(col)
 
+  tc <- query_data %>% total_counts(100, column = col)
 
-  ## Fix order of x and y variables
-  upset.cutoff <-upset %>% within(colname <- factor(colname, levels=names(sort(table(colname),
-                                                                                decreasing=TRUE))))
-  ## UpSetR plot
-  par(oma=c(5,5,5,5), mar=c(3,3,3,3))
-  upset(upset.cutoff[c(3,5:ncol(upset.cutoff))],	# text.scale=1.5,
-        sets=words.gecutoff$words, sets.bar.color="turquoise3",
-        main.bar.color="coral3",									#56B4E9 lightblue
-        group.by="degree", order.by=c("freq"),		# "degree"
-        mb.ratio=c(0.3, 0.7), # nintersects=20,
-        number.angles=0, point.size=2, line.size=0.8,
-        show.numbers="yes", shade.alpha = 0.25,
-        mainbar.y.label="Intersection counts",
-        sets.x.label="Individual set counts",
-        query.legend="top")
+  rows <- tc %>% select({{column}},'CumulativePercent') %>% distinct() #%>% arrange(CumulativePercent)
+
+  cutoff <- rows[rownumber,'CumulativePercent'] - .001
+
+  print(100-cutoff)
+
+  return(100-cutoff)
+}
+
+Queries_MaxPercent <- function(prot = all, queries, colname, RemoveAstrk = F)
+{
+  # Summarize by the queries and lineages, and record the max percentage for each query protein
+  # This allows easy conversions from Row Cutoffs to Query cutoffs
+  if(RemoveAstrk)
+  {
+    # Remove Asterisk if necessary
+    prot = remove_astrk(prot, colname)
+  }
+
+  # Total Count Summary
+  prot_tc <- prot %>% total_counts(column = colname, cutoff = 100)
+
+  colname = sym(colname)
+
+  query_max_percs = data.frame(Query = queries, maxPercent = double(length(queries)))
+  # Find largest cumulative percentage for each query
+  for(x in 1:length(queries))
+  {
+    q = queries[x]
+    q = sym(q)
+    # Filter by query
+    query_tc = prot_tc %>% filter(grepl(q,{{colname}}))
+
+    # Find max Percentage of the query
+    max_perc = max(query_tc$CumulativePercent)
+    query_max_percs$maxPercent[x] = max_perc
+  }
+
+  query_max_percs = query_max_percs %>% arrange(-maxPercent)
+
+  return(query_max_percs)
 }
 
 
 
+collapseInput <- function(inputId, boxId) {
+  #########
+  # Function to create input based off of if a box is collapsed or not
 
-
-
-# #########
-# ##MSA
-# #########
-# #This will create the Phylogenetic tree
-# #Pass the filepath of the fastafile as well as one of three plot types:
-# #1.apeTree , 2.ggTree, 3. msaTree
-# phylo.plots <- function(filepath, plot_type){
-#   fasta_filepath <- "data/alignments/pspn-duf3046-aln/pspn.31seq.aln.txt"
-#   my_seqs <- readAAStringSet(fasta_filepath)
-#   my_seqs_msa <- msa(my_seqs)
-#
-#   my_seqs_msa_aln <- msaConvert(my_seqs_msa, type="seqinr::alignment")
-#
-#   d <- dist.alignment(my_seqs_msa_aln, "identity")
-#   #  as.matrix(d)[2:5, "HBA1_Homo_sapiens", drop=FALSE]
-#
-#   ## Phylogenetic tree
-#   ## using package ape
-#   ## build neighbor-joining tree
-#   seqTree <- nj(d)
-#   if(plot_type== "apeTree"){
-#     plot(seqTree, main="Phylogenetic Tree of MSA")
-#   }
-#   else{
-#     ## drawing trees using ggtree
-#     #ggtree(seqTree)
-#     groupInfo <- split(seqTree$tip.label,
-#                        gsub("_\\w+", "", seqTree$tip.label))
-#     seqTree <- groupOTU(seqTree, groupInfo)
-#     if(plot_type == "ggTree"){
-#       ggtree(seqTree, aes(color=group),
-#              layout='circular') +
-#         geom_tiplab(size=1, aes(angle=angle))
-#     }
-#     else if(plot_type == "msaTree"){
-#       ## Tree + MSA
-#       # beast_file <- system.file("examples/MCC_FluA_H3.tree", package="ggtree")
-#       # beast_tree <- read.beast(beast_file)
-#       # fasta <- system.file("examples/FluA_H3_AA.fas", package="ggtree")
-#       # msaplot(ggtree(beast_tree), fasta)
-#       offs <- 0
-#       msaplot(ggtree(seqTree), fasta=fasta_filepath, offset=0.75) +
-#         geom_tiplab(size=2, align=TRUE, linesize=.5)
-#
-#     }
-#   }
-# }
+  tags$script(
+    sprintf(
+      "$('#%s').closest('.box').on('hidden.bs.collapse', function () {Shiny.onInputChange('%s', true);})",
+      boxId, inputId
+    ),
+    sprintf(
+      "$('#%s').closest('.box').on('shown.bs.collapse', function () {Shiny.onInputChange('%s', false);})",
+      boxId, inputId
+    )
+  )
+}
