3c857d73da9d513d1566b6ee4eaf78364d0f0535
Samuel Chen <chensam2@msu.edu>
2020-06-30 12:02:43 -0400

    Update evolvr Splash

--- evolvr/components.R
+++ evolvr/components.R
@@ -3,26 +3,49 @@ library(shinyBS) #modals
 library(shinyWidgets)
 
 
-diagram_box <- function(title, image, button_name, description)
+diagram_box <- function(title, image_name, button_name, description)
 {
   div(class = "diagram-box",
 
       div(title, class = "box-title"),
       div(description, class = "diagram-box-description"),
 
+
+      #div(class = "image-border", style = "border-style: solid; border-width: 0px 1px 1px 1px; border-color: #AAAAAA;",
+      div(class = "diagram-image",  style= paste0("background-image: url(", image_name, ".png);
+          background-size: auto 130%; background-position: center; background-repeat: no-repeat;")),
       actionButton(button_name, NULL, class="diagram-button")
 
 
 
-      )
-
-
-
+  )
 }
+
+diagram_elipse <- function(title, image)
+{
+
+  div(class = "diagram-elipse",
+      div(title, class = "elipse")
+
+      )
+}
+
 
 arrow_d <- function() {
   div(
-  icon(name = "arrow-right", class = "arrow arrow-d fa-3x", lib = "font-awesome"),
+    icon(name = "arrow-right", class = "arrow arrow-d fa-3x", lib = "font-awesome"),
+  )
+}
+
+arrow_r <- function() {
+  div(
+    icon(name = "arrow-right", class = "arrow arrow-r fa-3x", lib = "font-awesome"),
+  )
+}
+
+arrow_l <- function() {
+  div(
+    icon(name = "arrow-left", class = "arrow arrow-l fa-3x", lib = "font-awesome"),
   )
 }
 
@@ -33,4 +56,5 @@ arrow_dl <- function(){
 arrow_dr <- function(){
   icon(name = "arrow-right", class = "arrow arrow-dr fa-3x", lib = "font-awesome")
 }
+
 
--- evolvr/ui/splashPageUI.R
+++ evolvr/ui/splashPageUI.R
@@ -1,28 +1,48 @@
 
 tabPanel("Home", value = "home", icon = icon("home", class = "fa-1x", lib = "font-awesome"),
 fluidPage(
+  #### Input Query ####
   fluidRow(
-    column(width = 4, offset = 4,
+    column(width = 3, offset = 1,
            diagram_box("Query", image = "none", button_name = "dQueryBtn", description = "Description")
-    )),
-  fluidRow(
-    column(width = 4, offset = 4, arrow_d())
+    ),
+    column(width = 1, offset = 0,
+           arrow_r()
+    ),
+    column(width = 3, offset = 0,
+           diagram_box("Molecular Characterization of Target Protein", image = "none", button_name = "dMolTargetBtn", description = "Description")
+    ),
+    column(width = 1, offset = 0,
+           arrow_r()
+    ),
+    column(width = 3, offset = 0,
+           diagram_box("Identify Homologs", image = "none", button_name = "dHomoBtn", description = "Description")
+    )
   ),
   fluidRow(
-    column(width = 4, offset = 4,
-           diagram_box("Molecular Characterization", image = "none", button_name = "dMolecularBtn", description = "Description") )
+    column(width = 3, offset = 9, arrow_d())
   ),
   fluidRow(
-    column(width = 4, offset = 2, arrow_dl()),
-    column(width = 4, offset = 0, arrow_dr())
+    column(width = 3, offset = 5,
+           diagram_elipse("Analysis", image = "none") ),
+    column(width = 1, offset = 0, arrow_l()),
+    column(width = 3, offset = 0,
+           diagram_box("Molecular Characterization of Homologs", image = "none", button_name = "dMolHomoBtn", description = "Description") )
   ),
   fluidRow(
-    column(width = 4, offset = 0,
+    column(width = 2, offset = 4, arrow_dl()),
+    column(width = 1, offset = 0, arrow_d()),
+    column(width = 2, offset = 0, arrow_dr())
+  ),
+  fluidRow(
+    column(width = 3, offset = 2,
            diagram_box("Phyletic Spread", image = "none", button_name = "dPhyleticBtn", description = "Description")
     ),
-    column(width = 4, offset = 4,
+    column(width = 3, offset = 0,
+           diagram_box("Proximity Network", image = "none", button_name = "dNetworkBtn", description = "Description")
+    ),
+    column(width = 3, offset = 0,
            diagram_box("Structure Based Sequence Alignment", image = "none", button_name = "dAlignmentBtn", description = "Description")
     )
   )
-
 ))
