394f6899bae33c1a31c5e3a65d4d5b8d9668cbfa
Samuel Chen <chensam2@msu.edu>
2020-07-07 18:15:52 -0400

    Update splash page

--- evolvr/testSplash.R
+++ evolvr/testSplash.R
@@ -11,20 +11,23 @@ ui <-
       # setBackgroundImage(src = "the_approach_flowchart.png"),
       fluidRow(
         column(width = 3, offset = 1,
-               diagram_box("Query", image = "the_approach_flowchart", button_name = "dQueryBtn", description = "Description")
+               # diagram_box("Query", image = "None", button_name = "dQueryBtn", description = "Description"),
+               basic_box("Query Protein", image = "none", description = "Description", height = '70px'),
+               sub_box("Protein Accession Number", image_name = "none", description = "", button_name = "dAccNumBtn"),
+               sub_box("Fasta", image_name = "none", description = "", button_name = "dFastaBtn")
         ),
         column(width = 1, offset = 0,
                arrow_r()
-               ),
+        ),
         column(width = 3, offset = 0,
                diagram_box("Molecular Characterization of Target Protein", image = "mol_characterization", button_name = "dMolTargetBtn", description = "Description")
-               ),
+        ),
         column(width = 1, offset = 0,
                arrow_r()
-               ),
+        ),
         column(width = 3, offset = 0,
                diagram_box("Identify Homologs", image = "none", button_name = "dHomoBtn", description = "Description")
-               )
+        )
       ),
       fluidRow(
         column(width = 3, offset = 9, arrow_d())
@@ -48,7 +51,7 @@ ui <-
         ),
         column(width = 3, offset = 0,
                diagram_box("Proximity Network", image = "domain_network", button_name = "dNetworkBtn", description = "Description")
-               ),
+        ),
         column(width = 3, offset = 0,
                diagram_box("Structure Based Sequence Alignment", image = "phylo_tree", button_name = "dAlignmentBtn", description = "Description")
         )
@@ -99,6 +102,7 @@ ui <-
         column(width = 4, offset = 0,
                diagram_box("Structure Based Sequence Alignment", image = "none", button_name = "dAlignmentBtn", description = "Description")
         )
+
       )
 
 
--- evolvr/ui/splashPageUI.R
+++ evolvr/ui/splashPageUI.R
@@ -1,16 +1,20 @@
 
 tabPanel("Home", value = "home", icon = icon("home", class = "fa-1x", lib = "font-awesome"),
 fluidPage(
+  loginUI("login"),
   #### Input Query ####
   fluidRow(
     column(width = 3, offset = 1,
-           diagram_box("Query", image = "none", button_name = "dQueryBtn", description = "Description")
+           # diagram_box("Query", image = "None", button_name = "dQueryBtn", description = "Description"),
+           basic_box("Query Protein", image = "none", description = "Description", height = '70px'),
+           sub_box("Protein Accession Number", image_name = "none", description = "", button_name = "dAccNumBtn"),
+           sub_box("Fasta", image_name = "none", description = "", button_name = "dFastaBtn")
     ),
     column(width = 1, offset = 0,
            arrow_r()
     ),
     column(width = 3, offset = 0,
-           diagram_box("Molecular Characterization of Target Protein", image = "none", button_name = "dMolTargetBtn", description = "Description")
+           diagram_box("Molecular Characterization of Target Protein", image = "mol_characterization", button_name = "dMolTargetBtn", description = "Description")
     ),
     column(width = 1, offset = 0,
            arrow_r()
@@ -27,7 +31,7 @@ fluidPage(
            diagram_elipse("Analysis", image = "none") ),
     column(width = 1, offset = 0, arrow_l()),
     column(width = 3, offset = 0,
-           diagram_box("Molecular Characterization of Homologs", image = "none", button_name = "dMolHomoBtn", description = "Description") )
+           diagram_box("Molecular Characterization of Homologs", image = "mol_characterization", button_name = "dMolHomoBtn", description = "Description") )
   ),
   fluidRow(
     column(width = 2, offset = 4, arrow_dl()),
@@ -35,14 +39,15 @@ fluidPage(
     column(width = 2, offset = 0, arrow_dr())
   ),
   fluidRow(
+
     column(width = 3, offset = 2,
-           diagram_box("Phyletic Spread", image = "none", button_name = "dPhyleticBtn", description = "Description")
+           diagram_box("Phyletic Spread", image = "heatmap", button_name = "dPhyleticBtn", description = "Description")
     ),
     column(width = 3, offset = 0,
-           diagram_box("Proximity Network", image = "none", button_name = "dNetworkBtn", description = "Description")
+           diagram_box("Proximity Network", image = "domain_network", button_name = "dNetworkBtn", description = "Description")
     ),
     column(width = 3, offset = 0,
-           diagram_box("Structure Based Sequence Alignment", image = "none", button_name = "dAlignmentBtn", description = "Description")
+           diagram_box("Structure Based Sequence Alignment", image = "phylo_tree", button_name = "dAlignmentBtn", description = "Description")
     )
   )
 ))
