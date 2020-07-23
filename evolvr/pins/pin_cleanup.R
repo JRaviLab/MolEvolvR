805e0a7096649ea26a33157d004b69c164006f1c
Samuel Chen <chensam2@msu.edu>
2020-07-23 17:23:59 -0400

    Integrate pins with evolvr

---
+++ evolvr/pins/pin_cleanup.R
@@ -0,0 +1,54 @@
+library(lubridate)
+library(pins)
+
+pin_date <- function(name, board)
+{
+  # Retrieve the date that the pin was last modified
+  pin_recent_date <- pin_versions(name = name, board = board)$created[1]
+  return(as_datetime(pin_recent_date))
+}
+
+
+
+
+clean_pins <- function(board, expiration_time, timetype = "hours")
+{
+  #' Remove pins that have been pinned for longer than the expiration time
+  #'@author Samuel Chen
+  #'@param board Pin Board to clean
+  #'@param expiration_time The time limit that pins are allowed to remain
+  #'@param timetype String of the unit of the expiration time. Can be either
+  #'"hours", "minutes", "seconds", or "days". Defaults to hours
+  #'@return Integer value of the number of pins removed
+
+  # Convert expiration time to minutes
+  expiration_time <- switch(timetype,
+                            "hours" = expiration_time*3600,
+                            "minutes" = expiration_time*60,
+                            "days" = expiration_time * 24*3600,
+                            "seconds" = expiration_time
+  )
+
+  expiration_time <- make_difftime(expiration_time, units = "hours")
+
+  pin_names <- pin_find(board = "github")$name
+  # pin_get(board = "github")
+
+  current_time = as_datetime(Sys.time())
+
+
+  removed = 0
+  for(pin in pin_names)
+  {
+    if(current_time - pin_date(pin, board) > expiration_time )
+    {
+      pin_remove(name = pin, board = board)
+      removed = removed + 1
+    }
+
+  }
+  return(removed)
+
+}
+
+
