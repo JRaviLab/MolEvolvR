007267f1eb9940bccadfc0f5da27eed1bd580dd2
Samuel Chen <chensam2@msu.edu>
2020-08-07 13:53:55 -0400

    Fix Network

--- R/network.R
+++ R/network.R
@@ -6,13 +6,14 @@
 ## Pkgs needed ##
 #################
 library(tidyverse)
+library(rlang)
 library(igraph)
 library(visNetwork)
 conflicted::conflict_prefer("filter", "dplyr")
 ###########################
 #### Network FUNCTIONS ####
 ###########################
-domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff = 70, layout = "nice", UsingRowsCutoff = F){
+domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff = 70, layout = "nice", query_color = adjustcolor("green", alpha.f = .5)){
   #'Domain Network
   #'
   #'This function creates a domain network from the 'DomArch' column.
@@ -34,13 +35,15 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
 
   column_name <- sym(column)
 
-  prot_tc <- prot %>% total_counts(column =  column, cutoff = cutoff, RowsCutoff = UsingRowsCutoff, digits = 5)
+  prot_tc <- prot %>% total_counts(column =  column, cutoff = cutoff, RowsCutoff = F, digits = 5)
 
   within_list <- prot_tc %>% select({{column_name}}) %>% distinct()
   within_list <- pull(within_list, {{column_name}})
-
+  #################### Why is below here?
+  ######## nvm, it's making sure that only Domains that are in the tc cutoff range are kept
   prot <- prot %>% filter({{column_name}} %in% within_list)
 
+  ####### Below should be part of the standardized cleanup process
   prot$DomArch.ntwrk <- as_vector(prot %>% select({{column}})) %>% # testing with prot$DomArch.orig
     str_replace_all(coll(pattern="\\?", ignore_case=T), "X")
 
@@ -50,27 +53,24 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
   #ye=grep(pattern = dom,x = prot.list,value = T)
   #ye=unlist(strsplit(ye,"\\+"))
   domains_of_interest_regex = paste(domains_of_interest, collapse = "|")
-  ye <- prot %>%
+  domain.list <- prot %>%
     dplyr::filter(grepl(pattern=domains_of_interest_regex,
                         x=DomArch.ntwrk,
                         ignore.case=T))
 
   ##Separating column and converting to atomic vector prevents coercion
-  ye <- ye$DomArch.ntwrk  %>% str_split(pattern="\\+")
-  # Domain network
-  domain.list <- ye#strsplit(ye,"\\+") #[[3]] or whatever index of the list(ye) contains domntwk
-  if(any(lengths(domain.list)==1)) {
-    domain.list=domain.list[-which(lengths(domain.list)==1)]
-  }
+  domain.list <- domain.list$DomArch.ntwrk  %>% str_split(pattern="\\+")
 
+  # Get domain counts before eliminating domarchs with no edges
+  wc = elements2words(prot = prot, column =  column, conversion_type = "da2doms") %>% words2wc()
+  wc = pivot_wider(wc, names_from = words, values_from = freq)
+
+  # Remove all isolated domarchs, such that an adjacency list can easily be constructed
+  domain.list = domain.list[-which(lengths(domain.list)==1)]
 
   # This is where we know if the adjacency list is empty
   if(length(domain.list) == 0)
   {
-    wc = elements2words(prot = prot, column =  column, conversion_type = "da2doms") %>% words2wc()
-
-    wc = pivot_wider(wc, names_from = words, values_from = freq)
-
     g <- make_empty_graph()
     # Add nodes included in domains of interest
     g <- g  + vertices(domains_of_interest)
@@ -110,17 +110,34 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
     }
     pwise.ass <- sapply(1:length(pwise[,1]), function(x) paste(pwise[x,],collapse = "->"))
     e.sz <- sort(table(pwise.ass),decreasing = T)
-    v.sz <- sort(table(pwise),decreasing = T)
+    # v.sz <- sort(table(pwise),decreasing = T)
     e.sz <- sort(table(pwise.ass),decreasing = T)  # Edge weights
-    v.sz <- sort(table(pwise),decreasing = T)   # Vertex weights
+    # v.sz <- sort(table(pwise),decreasing = T)   # Vertex weights
     pwise <- strsplit(names(e.sz), "\\->")
     pwise <- cbind(unlist(lapply(pwise, function(x) x[1])),unlist(lapply(pwise, function(x) x[2])))
 
     # Plotting the network
     g <- graph_from_edgelist(pwise, directed=TRUE)
     # scaling vertex size
-    V(g)$size <- v.sz[V(g)$name]
+    #V(g)$size <- v.sz[V(g)$name]
+
+
+    # # Add query domains if not already present b/c they have not adjacency add them to the graph if not already present
+    for(domain in domains_of_interest)
+    {
+      # domain_size = as.numeric(wc[1,domain])
+      if( (!domain %in% V(g)$name) && (domain %in% colnames(wc)) )
+      {
+        g <- g + vertex(domain)
+      }
+    }
+
+
+    V(g)$size <- as.numeric(wc[V(g)$name])
+
     V(g)$size <- (V(g)$size-min(V(g)$size))/(max(V(g)$size)-min(V(g)$size))*20+10 # scaled by degree
+
+
     # setting vertex color by size
     V(g)$color <- rainbow(5,alpha = .5)[round((V(g)$size-min(V(g)$size))/(max(V(g)$size)-min(V(g)$size))*4+1)]
     V(g)$frame.color <- V(g)$color
@@ -130,29 +147,28 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
     # coloring edges by width
     ew <- c(2.7,4.5)
     E(g)$color <- sapply(E(g)$width,
-                         function(x) if(x>=ew[1] && x<=ew[2]) E(g)$color="cadetblue" else if(x>ew[2]) E(g)$color="maroon" else E(g)$color="gray55")
+                         function(x) if(x>=ew[1] && x<=ew[2]) E(g)$color=adjustcolor("cadetblue", alpha.f = .7) else if(x>ew[2]) E(g)$color=adjustcolor("maroon", alpha.f = .5) else E(g)$color="gray55")
   }
 
 
+  #   # V(g)[which(V(g)$name %in% domains_of_interest)]$size <- as.numeric(wc[domains_of_interest])
+  V(g)[which(V(g)$name %in% domains_of_interest)]$color = query_color
 
 
+  vis_g = visIgraph(g, type ="full")
 
-  # par(mar=c(2.5, 2, 2, 1))
-  # switch(layout,
-  #        "random" = plot.igraph(g,layout = layout_randomly(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black", edge.arrow.size = .1),
-  #        "grid" = plot.igraph(g,layout = layout_on_grid(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black", edge.arrow.size = .1),
-  #        "circle" = plot.igraph(g,layout = layout.circle(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black", edge.arrow.size = .1),
-  #        "auto" =plot.igraph(g,layout = layout.auto(g),vertex.label.dist=0,asp=0,edge.curved=F, vertex.label.color="black", edge.arrow.size = .1)
-  # )
+  max_font_size = 43
 
-  g = visIgraph(g, type ="full")
+  vis_g$x$nodes$font.size <- purrr::map(vis_g$x$nodes$size, function(x) min(x*2 , max_font_size))
 
-  switch(layout,
-         "nice" = visIgraphLayout(g,"layout_nicely"),
-         "random" = visIgraphLayout(g, "layout_randomly"),
-         "grid" = visIgraphLayout(g, "layout_on_grid"),
-         "circle" = visIgraphLayout(g, "layout.circle"),
-         "auto" =  visIgraphLayout(g, "layout.auto")
+  vis_g <- switch(layout,
+                  "nice" = visIgraphLayout(vis_g,"layout_nicely", ),
+                  "random" = visIgraphLayout(vis_g, "layout_randomly"),
+                  "grid" = visIgraphLayout(vis_g, "layout_on_grid"),
+                  "circle" = visIgraphLayout(vis_g, "layout.circle"),
+                  "auto" =  visIgraphLayout(vis_g, "layout.auto")
   )
-
+  vis_g <- vis_g %>%
+    visOptions(highlightNearest = TRUE)
+  vis_g
 }
