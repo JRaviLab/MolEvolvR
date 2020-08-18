b2f719d2d1b591a7c4e725c2e5102db61a45e7ec
Samuel Chen <chensam2@msu.edu>
2020-08-18 11:12:41 -0400

    Create BinaryNetwork

--- R/network.R
+++ R/network.R
@@ -172,3 +172,127 @@ domain_network <- function(prot, column = "DomArch", domains_of_interest, cutoff
     visOptions(highlightNearest = TRUE)
   vis_g
 }
+
+BinaryDomainNetwork <- function(prot, column = "DomArch", domains_of_interest, cutoff = 70,
+                                layout = "nice", query_color = adjustcolor("yellow", alpha.f = .5),
+                                partner_color = adjustcolor("skyblue", alpha.f = .5),
+                                border_color = adjustcolor("grey", alpha.f = .8),
+                                IsDirected = T
+){
+  #'Domain Network
+  #'
+  #'This function creates a domain network from the 'DomArch' column.
+  #'
+  #'Domains that are part of the 'domains_of_interest' are a different node color than the other domains.
+  #'
+  #'A network of domains is returned based on shared domain architectures.
+  #'
+  #'@param prot A data frame that contains the column 'DomArch'.
+  #'@param column Name of column containing Domain architecture from which nodes and edges are generated.
+  #'@param cutoff Integer. Only use domains that occur at or above the cutoff for total counts if cutoff_type is "Total Count".
+  #'Only use domains that appear in cutoff or greater lineages if cutoff_type is Lineage.
+  #'@param layout Character. Layout type to be used for the network. Options are:
+  #'\itemize{\item "grid" \item "circle" \item "random" \item "auto"}
+  #'@param query_color Color that the nodes of the domains in the domains_of_interest vector are colored
+  #'@param partnercolor Color that the nodes that are not part of the domains_of_interest vector are colored
+  #'@param IsDirected Is the network directed? Set to false to eliminate arrows
+  #'@examples domain_network(pspa)
+  # by domain networks or all, as required.
+  print(domains_of_interest)
+
+  column_name <- sym(column)
+
+  prot_tc <- prot %>% total_counts(column =  column, cutoff = cutoff, RowsCutoff = F, digits = 5)
+
+  within_list <- prot_tc %>% select({{column_name}}) %>% distinct()
+  within_list <- pull(within_list, {{column_name}})
+
+  prot <- prot %>% filter({{column_name}} %in% within_list)
+
+  ####### Below should be part of the standardized cleanup process
+  prot$DomArch.ntwrk <- as_vector(prot %>% select({{column}})) %>% # testing with prot$DomArch.orig
+    str_replace_all(coll(pattern="\\?", ignore_case=T), "X")
+
+  domains_of_interest_regex = paste(domains_of_interest, collapse = "|")
+  domain.list <- prot %>%
+    dplyr::filter(grepl(pattern=domains_of_interest_regex,
+                        x=DomArch.ntwrk,
+                        ignore.case=T))
+
+  ##Separating column and converting to atomic vector prevents coercion
+  domain.list <- domain.list$DomArch.ntwrk  %>% str_split(pattern="\\+")
+
+  # Get domain counts before eliminating domarchs with no edges
+  wc = elements2words(prot = prot, column =  column, conversion_type = "da2doms") %>% words2wc()
+
+  nodes = data.frame(id = wc$words, label = wc$words, size = wc$freq) %>%
+    mutate(group = purrr::map(id,
+                              function(x)
+                                ifelse(x %in% domains_of_interest, "Query", "Partner")))
+
+  max_size = max(nodes$size)
+  min_size = min(nodes$size)
+  nodes <- nodes %>% mutate(size = (size-min_size)/((max_size-min_size)) *20+10)
+  max_font_size = 43
+  nodes <- nodes %>% mutate(font.size = purrr::map(size, function(x) min(x*2 , max_font_size)))
+
+  domain.list = domain.list[-which(lengths(domain.list)==1)]
+
+  if(length(domain.list) != 0)
+  {
+    from <- unlist(lapply(domain.list, function(x) sapply(1:(length(x)-1), function(y) x[y]))) #list elements 1 through n-1
+    to <- unlist(lapply(domain.list, function(x) sapply(1:(length(x)-1), function(y) x[y+1]))) #list elements 2 through n
+    pwise <- cbind(from,to)
+    if(any(pwise=="")) {
+      pwise=pwise[-which((pwise[,1]=="") | pwise[,2]==""),]
+    }
+    edges <- data.frame(from = pwise[,1], to = pwise[,2]) %>%
+      group_by(from, to) %>% summarize(width = n())
+    edges <- edges %>% mutate(width = ifelse(width==1, .3, log(width)))
+    ew <- c(2.7,4.5)
+
+    ColorEdges <- function(x)
+    {
+      if(x>=ew[1] && x<=ew[2])
+      {
+        adjustcolor("cadetblue", alpha.f = .7)
+      }
+      else if(x>ew[2])
+      {
+        adjustcolor("maroon", alpha.f = .5)
+      }
+      else
+      {
+        "gray55"
+      }
+    }
+
+    edges <- edges %>% mutate(color = unlist(purrr::map(width, ColorEdges)) )
+
+  }
+
+  if(IsDirected){
+    vg <- visNetwork(nodes, edges, width = "100%", height = '600px')  %>%
+      visEdges(arrows = 'to', smooth =T)
+  }
+  else
+  {
+    vg <- visNetwork(nodes, edges, width = "100%", height = '600px')  %>%
+      visEdges(smooth =T)
+  }
+  vg <- vg %>%   visGroups(groupname = "Query", color = query_color) %>%
+    visGroups(groupname = "Partner", color = partner_color) %>%
+    visOptions(highlightNearest = TRUE) %>%
+    visLegend()
+
+  vg <- switch(layout,
+               "nice" = visIgraphLayout(vg, "layout_nicely" ),
+               "random" = visIgraphLayout(vg, "layout_randomly"),
+               "grid" = visIgraphLayout(vg, "layout_on_grid"),
+               "circle" = visIgraphLayout(vg, "layout.circle"),
+               "auto" =  visIgraphLayout(vg, "layout.auto")
+  )
+  vg
+}
+
+
