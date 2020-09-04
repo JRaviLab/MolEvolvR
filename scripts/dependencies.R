793d69acc2237c2be554c8dfd2575b103243b4d3
Samuel Chen <chensam2@msu.edu>
2020-09-04 13:12:41 -0400

    Update dependencies

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -3,7 +3,21 @@ install.packages(c("manipulateWidget", "quadprog", "fastmatch", "maps", "gtools"
 install.packages(c("phytools", "phangorn"), dependencies=T)
 
 ## Other packages
-install.packages(c("conflicted","here","tidyverse","pdftools","latexpdf","UpSetR","wordcloud"), dependencies=T)
-install.packages(c("gggenes", "ggraph", "igraph", "ggdendro", "gganimate", "ggnetwork", "networkD3", "geomnet", "ggrepel", "cowplot", "ggtree"), dependencies=T)
+install.packages(c("conflicted","here","tidyverse","pdftools","latexpdf","UpSetR","wordcloud", "rlang"), dependencies=T)
+install.packages(c("gggenes", "ggraph", "igraph", "visNetwork", "ggdendro", "gganimate", "ggnetwork", "networkD3", "geomnet", "ggrepel", "cowplot", "ggtree"), dependencies=T)
 
+install.packages(c("shinyBS", "pins", "shinyjs", "sunburstR", "sodium", "rentrez"))
+
+install.packages(c("viridis", "msa", "furrr"))
+
+{if(Sys.info()["sysname"] == "Windows"){
+  install.packages("XML", type =  "win.binary")
+}
+else{
+  install.packages("XML")
+}}
+install.packages("rentrez")
+
+
+remotes::install_github("paulc91/shinyauthr")
 remotes::install_github("mhahsler/rMSA", force=T)
