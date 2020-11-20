e68b8fc577f24375fb33490c4215e433de5726ca
Samuel Chen <chensam2@msu.edu>
2020-11-20 11:46:21 -0500

    combine wordcloud3.R and plotting.R

--- R/plotting.R
+++ R/plotting.R
@@ -18,9 +18,6 @@ library(sunburstR)
 library(d3r)
 library(viridis)
 
-
-source("R/wordcloud3.R")
-
 shorten_lineage <- function(data, colname = "Lineage")
 {
   abbrv <- function(x){
@@ -449,6 +446,54 @@ lineage.domain_repeats.plot <- function(query_data, colname) {
 ## Wordclouds ##
 ################
 #### NEEDS SOME WORK
+
+
+# Modified script of wordcloud2
+wordcloud3 <- function (data, size = 1, minSize = 0, gridSize = 0, fontFamily = "Segoe UI",
+                        fontWeight = "bold", color = "random-dark", backgroundColor = "white",
+                        minRotation = -pi/4, maxRotation = pi/4, shuffle = TRUE,
+                        rotateRatio = 0.4, shape = "circle", ellipticity = 0.65,
+                        widgetsize = NULL, figPath = NULL, hoverFunction = NULL) {
+  if ("table" %in% class(data)) {
+    dataOut = data.frame(name = names(data), freq = as.vector(data))
+  }
+  else {
+    data = as.data.frame(data)
+    dataOut = data[, 1:3]
+    names(dataOut) = c("name", "freq", "label")
+  }
+  if (!is.null(figPath)) {
+    if (!file.exists(figPath)) {
+      stop("cannot find fig in the figPath")
+    }
+    spPath = strsplit(figPath, "\\.")[[1]]
+    len = length(spPath)
+    figClass = spPath[len]
+    if (!figClass %in% c("jpeg", "jpg", "png", "bmp", "gif")) {
+      stop("file should be a jpeg, jpg, png, bmp or gif file!")
+    }
+    base64 = base64enc::base64encode(figPath)
+    base64 = paste0("data:image/", figClass, ";base64,",
+                    base64)
+  }
+  else {
+    base64 = NULL
+  }
+  weightFactor = size * 180/max(dataOut$freq)
+  settings <- list(word = dataOut$name, freq = dataOut$freq, label = dataOut$label,
+                   fontFamily = fontFamily, fontWeight = fontWeight, color = color,
+                   minSize = minSize, weightFactor = weightFactor, backgroundColor = backgroundColor,
+                   gridSize = gridSize, minRotation = minRotation, maxRotation = maxRotation,
+                   shuffle = shuffle, rotateRatio = rotateRatio, shape = shape,
+                   ellipticity = ellipticity, figBase64 = base64, hover = htmlwidgets::JS(hoverFunction))
+  chart = htmlwidgets::createWidget("wordcloud2", settings,
+                                    width = widgetsize[1], height = widgetsize[2], sizingPolicy = htmlwidgets::sizingPolicy(viewer.padding = 0,
+                                                                                                                            browser.padding = 0, browser.fill = TRUE))
+
+  chart
+  #htmlwidgets::onRender(chart, "function(el,x){\n                        console.log(123);\n                        if(!iii){\n                          window.location.reload();\n                          iii = False;\n\n                        }\n  }")
+}
+
 
 wordcloud_element <- function(query_data="prot",
                               colname = "DomArch",
