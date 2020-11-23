ea6d1958ddaf77b88abc26496f5a56e37032fe7b
laurensosinski <sosinsk7@msu.edu>
2020-11-23 11:03:22 -0500

    update wrapper script ipr2da call -- add suffix variable; update ipr2da rm local paths completely as well as default calls to variables

--- upstream_scripts/05a_ipr2da.R
+++ upstream_scripts/05a_ipr2da.R
@@ -3,29 +3,17 @@ library(data.table)
 library(furrr)
 
 # compute cvm location for acc2lin.R
-#source("/data/research/jravilab/laurensosinski/scripts/acc2lin.R")
-# local location for acc2lin.R
-source("scripts/acc2lin.R")
+source("/data/research/jravilab/molevol_scripts/upstream_scripts/acc2lin.R")
+
 
 ## load file in
-# on HPCC
 args <- commandArgs(trailingOnly = TRUE)
-
-# locally
-# args <- c("data/molevolvr_outputs/WP_020839904.1_Vibrio_parahaemolyticus_out/WP_020839904.1_Vibrio_parahaemolyticus.iprscan.tsv",
-#           "data/molevolvr_outputs/WP_020839904.1_Vibrio_parahaemolyticus_out/WP_020839904.1_Vibrio_parahaemolyticus.refseq.1e-5.cln.txt")
 
 # ipr2da function
 ipr2da <- function(infile_ipr, infile_blast, suffix, analysis=c("Pfam","SMART", "CDD", "TIGRFAM",
                                                         "Phobius", "Gene3D", "TMHMM", "SignalP_EUK",
                                                         "SignalP_GRAM_NEGATIVE", "SignalP_GRAM_POSITIVE"))
   {
-  infile_ipr = "data/molevolvr_outputs/slps/ADY20190.1_EA1_Bacillus_thuringiensis_out/ADY20190.1_EA1_Bacillus_thuringiensis.iprscan.tsv"
-  infile_blast = "data/molevolvr_outputs/slps/ADY20190.1_EA1_Bacillus_thuringiensis_out/ADY20190.1_EA1_Bacillus_thuringiensis.refseq.1e-5.cln.txt"
-  analysis=c("Pfam","SMART", "CDD", "TIGRFAM", "Phobius", "Gene3D", "TMHMM", "SignalP_EUK",
-             "SignalP_GRAM_NEGATIVE", "SignalP_GRAM_POSITIVE")
-  suffix = "ADY20190.1_EA1_Bacillus_thuringiensis"
-
   # creating column names for input files and lookup tables
   ipr_colnames <- c("AccNum", "SeqMD5Digest", "SLength", "Analysis",
                     "DB.ID", "SignDesc", "StartLoc", "StopLoc", "Score",
@@ -83,7 +71,7 @@ ipr2da <- function(infile_ipr, infile_blast, suffix, analysis=c("Pfam","SMART", 
 
   # TaxID to lineage
   blast_out$TaxID <- as.integer(blast_out$TaxID)
-  lineage_map <- fread("data/ReferenceFiles/lineagelookup.txt", sep = "\t")
+  lineage_map <- fread("/data/research/jravilab/common_data/lineagelookup.txt", sep = "\t")
   # get lineage path as argument, it'll be changed depending on who is running it
   # have default argument also for where shit is
   mergedLins <- merge(blast_out, lineage_map, by.x = "TaxID", by.y="tax_id", all.x = T)
