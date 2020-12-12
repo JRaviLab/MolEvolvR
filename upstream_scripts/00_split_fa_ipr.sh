eaaef82ebde36bd208f5f4f547829cc4bb68ab9c
laurensosinski <sosinsk7@msu.edu>
2020-12-12 12:43:37 -0500

    00_split_full.sh created by @jananiravi to split multifasta files, append file paths to text file, and submit the wrapper script; split_fa_ipr created as precursor to to split_full; 00_wrapper_ipr.sb created to run analysis starting with iprscan outputs

---
+++ upstream_scripts/00_split_fa_ipr.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## USER INPUTS
+INFILE=$1
+DB=refseq
+NHITS=5000
+EVAL=1e-5
+
+dir=$(dirname $INFILE)
+base=$(basename $INFILE)
+cd ${dir}
+
+
+NFASTA=$(grep "^>" $INFILE | wc -l)
+
+
+if [ $NFASTA -gt 1 ]
+then
+   printf "No. of seqs provided: $NFASTA>1\nSo, we are going to split it up for you prior to the analysis.\n"
+   sh /data/research/jravilab/molevol_scripts/upstream_scripts/split_fasta_jr.sh ${INFILE}
+   find $PWD -type f -name "*.faa" > input.txt
+
+elif [ $NFASTA = 1 ] 
+then
+   printf "No. of seqs provided: $NFASTA=1\nSo, we are going to proceed to the analysis.\n"
+   find $PWD -type f -name "$base" > input.txt
+fi
+
+INPATHS=input.txt
+cat $INPATHS
+qsub /data/research/jravilab/molevol_scripts/upstream_scripts/00_wrapper.sb -F $INPATHS
