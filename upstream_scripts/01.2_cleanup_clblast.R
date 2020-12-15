5bbcf7a31c254842861918b787b37f4b35d318d8
laurensosinski <sosinsk7@msu.edu>
2020-12-14 22:03:20 -0500

    allow blast cleanup script to accept either web blast or command line blast inputs

--- upstream_scripts/01.2_cleanup_clblast.R
+++ upstream_scripts/01.2_cleanup_clblast.R
@@ -6,48 +6,53 @@ library(data.table)
 
 # compute cvm location for acc2lin.R
 source("/data/research/jravilab/molevol_scripts/R/pre-msa-tree.R")
+source("/data/research/jravilab/molevol_scripts/R/colnames_molevol.R")
 
 ## Read in data file path as a string
 args <- commandArgs(trailingOnly = TRUE)
 
 ## clean up function
 # takes in path to blast result file as a string
-cleanup_clblast <- function(infile_blast) {
-
-  # column names for blast results
-  cl_blast_cols <- c("Query", "SAccNum", "AccNum", "SAllSeqID", "STitle", "Species", "TaxID",
-                     "PcIdentity", "AlnLength", "Mismatch", "Gapopen","QStart", "QEnd",
-                     "QLength", "SStart", "SEnd", "SLength", "EValue", "BitScore",
-                     "PcPosOrig")
+cleanup_clblast <- function(infile_blast, wblast) {
 
   # read in blast results from input variable, set column names to blast_cols (created above)
-  blast_out <- read_tsv(file = infile_blast, col_names = cl_blast_cols)
-
-  cleanedup_blast <- blast_out %>%
-    # remove extra characters/names from sseqid, sscinames, and staxids columns
-    mutate(AccNum = gsub('\\|', '', AccNum)) %>%
-    mutate(AccNum = gsub('.*[a-z]', '', AccNum)) %>%
-    mutate(Species = gsub(';.*$', '', Species)) %>%
-    mutate(TaxID = gsub(';.*$', '', TaxID)) %>%
-    mutate(PcIdentity = round(PcIdentity, 2)) %>%
-    # normalize percent positive by multiplying the original ppos by the length of the subject protein
-    #   length and dividing by the query protein length
-    mutate(PcPositive = round(x = (PcPosOrig * AlnLength/QLength), digits = 2))
+  if (wblast == T) {
+     blast_out <- read_tsv(file = infile_blast, col_names = web_blast_colnames)
+     cleanedup_blast <- blast_out %>%
+       # remove extra characters/names from sseqid, sscinames, and staxids columns
+       mutate(AccNum = gsub('\\|', '', AccNum)) %>%
+       mutate(AccNum = gsub('.*[a-z]', '', AccNum)) %>%
+       mutate(TaxID = gsub(';.*$', '', TaxID)) %>% 
+       mutate(PcIdentity = round(PcIdentity, 2))
+  } else if (wblast == F) {
+     blast_out <- read_tsv(file = infile_blast, col_names = cl_blast_colnames)
+     cleanedup_blast <- blast_out %>%
+       # remove extra characters/names from sseqid, sscinames, and staxids columns
+       mutate(AccNum = gsub('\\|', '', AccNum)) %>%
+       mutate(AccNum = gsub('.*[a-z]', '', AccNum)) %>%
+       mutate(Species = gsub(';.*$', '', Species)) %>%
+       mutate(TaxID = gsub(';.*$', '', TaxID)) %>%
+       mutate(PcIdentity = round(PcIdentity, 2)) %>%
+       # normalize percent positive by multiplying the original ppos by the length of the subject protein
+       #   length and dividing by the query protein length
+       mutate(PcPositive = round(x = (PcPosOrig * AlnLength/QLength), digits = 2))
+  }
 
   # # TaxID to lineage
   cleanedup_blast$TaxID <- as.integer(cleanedup_blast$TaxID)
-  lineage_map <- fread("/data/research/jravilab/common_data/lineagelookup.txt", sep = "\t")
+  lineage_map <- fread("/data/research/jravilab/common_data/lineage_lookup.txt", header = T, fill = T)
   # # get lineage path as argument, it'll be changed depending on who is running it
   # # have default argument also for where the files are located
-  mergedLins <- merge(cleanedup_blast, lineage_map, by.x = "TaxID", by.y="TaxID", all.x = T)
+  mergedLins <- merge(cleanedup_blast, lineage_map, by = c('Species', 'TaxID'), all.x = T) %>%
+    select(all_of(cl_blast_postcln_cols))
   
   blast_names <- add_name(mergedLins)
 
   # create name for new output file to be created
   file_name <- gsub(pattern = '.txt', replacement = '', x = infile_blast) %>%
-    paste0('.cln.txt')
+    paste0('.cln.tsv')
   # write the cleaned up data to new file
   write_tsv(blast_names, file_name, col_names = T)
 }
 
-cleanup_clblast(args[1])
+cleanup_clblast(args[1], args[2])
