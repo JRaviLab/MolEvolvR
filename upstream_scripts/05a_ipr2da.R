3e203654e9a967853dbbebeb0907cf8a1ac5ca8d
laurensosinski <sosinsk7@msu.edu>
2020-12-19 17:03:18 -0500

    troubleshooting ipr2da
    ipr2da having trouble with merging domarch2 and acc2info, testing locally

--- upstream_scripts/05a_ipr2da.R
+++ upstream_scripts/05a_ipr2da.R
@@ -3,34 +3,30 @@ library(data.table)
 library(furrr)
 
 # source lineage.R
-# source("/data/research/jravilab/molevol_scripts/R/pre-msa-tree.R")
+source("/data/research/jravilab/molevol_scripts/R/colnames_molevol.R")
 
 ## load files in
-args <- commandArgs(trailingOnly = TRUE)
-
 # ipr2da function
-ipr2da <- function(infile_ipr, suffix, analysis=c("Pfam","SMART", "CDD", "TIGRFAM",
+ipr2da <- function(infile_ipr, acc2info, prefix, analysis=c("Pfam","SMART", "CDD", "TIGRFAM",
                                                   "Phobius", "Gene3D", "TMHMM", "SignalP_EUK",
                                                   "SignalP_GRAM_NEGATIVE", "SignalP_GRAM_POSITIVE"))
   {
-  # creating column names for input files and lookup tables
-  ipr_colnames <- c("AccNum", "SeqMD5Digest", "SLength", "Analysis",
-                    "DB.ID", "SignDesc", "StartLoc", "StopLoc", "Score",
-                    "Status", "RunDate", "IPRAcc", "IPRDesc")
-
+  # read in lookup table
   lookup_tbl <- fread("/data/research/jravilab/common_data/cln_lookup_tbl.tsv", header = T, fill = T) %>%
     arrange() %>% distinct()
 
-  # read in blast results
-  #blast_out <- fread(infile_blast, header = T, keepLeadingZeros = T)
-  
-  # read in iprscan results,
-  ipr_in <- read_tsv(infile_ipr, col_names = ipr_colnames) %>%
+  # read in iprscan results
+  ipr_in <- fread(file = infile_ipr, sep = "\t", header = F, col.names = ipr_colnames, fill = T) %>%
     mutate(DB.ID = gsub('G3DSA:', '', DB.ID))
 
-  prot_in_da <- ipr_in %>%
-    merge(lookup_tbl, by = "DB.ID", all.x = T)
+  # read in acc2info
+  acc2info_out <- fread(file = acc2info, sep = '\t', header = T, fill = T) %>%
+    mutate(FullAccNum = gsub('\\|', '', FullAccNum)) %>%
+    transmute(AccNum = gsub('.*[a-z]', '', FullAccNum))
 
+  prot_in_da <- ipr_in %>%
+    merge.data.table(y = lookup_tbl, by.x = "DB.ID", by.y = "DB.ID", all.x = T)
+  
   # split dataframe into unique proteins
   x <- split(x = prot_in_da, f = prot_in_da$AccNum)
 
@@ -47,7 +43,7 @@ ipr2da <- function(infile_ipr, suffix, analysis=c("Pfam","SMART", "CDD", "TIGRFA
       if (a == "SignalP_EUK" || a == "SignalP_GRAM_NEGATIVE" || a == "SignalP_GRAM_POSITIVE") {
         var_shortname = "DB.ID" }
       else {
-        var_shortname = "Short.Name" }
+        var_shortname = "ShortName" }
       var_shortname_sym = sym(var_shortname)
       a_da <- a_da %>%
         ungroup() %>%
@@ -63,32 +59,59 @@ ipr2da <- function(infile_ipr, suffix, analysis=c("Pfam","SMART", "CDD", "TIGRFA
     colnames(DAs) = paste("DomArch", analysis, sep = ".")
     return(cbind(acc_row, DAs))
   })
+  
+  # read in lineage mapping file
+  lineage_map <- fread("/data/research/jravilab/common_data/lineage_lookup.txt", header = T, fill = T)
+ 
+  head(lineage_map)
 
-  domarch2 <- do.call(rbind.data.frame, domarch)
+  head(domarch)
+
+  # combine domarchs to one data frame, merge w/ acc2info
+  domarch2 <- do.call(rbind.data.frame, domarch) #%>%
+    #merge.data.table(acc2info_out, by = "AccNum")
   
-  write_tsv(domarch2, file = paste0(suffix, '.ipr_domarch.tsv'), append = F, na = 'NA')
+  head(domarch2)
+
+  # add lineage to domarch, remove extra species column
+  domarch_lins <- merge.data.table(domarch2, lineage_map, by = "TaxID", all.x = T) #%>%
+   #select(-Species.x)
   
+  head(domarch_lins)
+  
+  # change Species.y colname to Species
+  names(ipr_lin)[names(ipr_lin) == 'Species.y'] <- 'Species'
+  # add name column to domarch+lineage dataframe
+  domarch_lins <- domarch_lins %>% add_name()
+
+  # save domarch_lins file
+  write_tsv(domarch_lins, file = paste0(prefix, '.ipr_domarch.tsv'), append = F, na = 'NA')
+  
+  # return domarch2 dataframe to append to blast results if given
   return(domarch2)
 }
 
 ## function for adding results from ipr2da to blast results
 
-append_ipr <- function(ipr_da, blast, suffix) {
+append_ipr <- function(ipr_da, blast, prefix) {
   ipr_domarch <- fread(ipr_da, header = T, fill = T)
   blast_out <- fread(blast, header = T, fill = T)
   
   blast_ipr <- merge (blast_out, ipr_domarch, by = 'AccNum')
-  write_tsv(blast_ipr, file = paste0(suffix, '.cln.clust.ipr.tsv'), na = 'NA')
+  write_tsv(blast_ipr, file = paste0(prefix, '.cln.clust.ipr.tsv'), na = 'NA')
 }
 
+# accept command line args
+args <- commandArgs(trailingOnly=TRUE)
+
 ## perform ipr2da on iprscan results
-da <- ipr2da(args[1],args[2])
+da <- ipr2da(infile_ipr = args[1], acc2info =  args[2], prefix = args[3])
 
 ## if blast results are provided, call append_ipr
-if (is.null(args[3]) | length(args[3] == 0)) {
+if (is.null(args[4]) | length(args[4] == 0)) {
   print('No blast results provided, moving on.') 
-} else if (args[3] == grep('.cln.clust.txt', args[3])) {
-  append_ipr(da, blast=args[3], suffix=args[2]) 
+} else if (args[4] == grep('.cln.clust.tsv', args[4])) {
+  append_ipr(da, blast=args[4], prefix=args[3]) 
 }
 
 
