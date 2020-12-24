c69e927da8275dd0a340917c19d7c3b073d143b1
Janani Ravi <janani@msu.edu>
2020-12-23 20:40:53 -0500

    cleaned up ipr2viz
    @samuelzornchen @laurensosinski
    - renamed functions, variables
    - default is Name
    - one func for our cln IPRSCAN files and one for web IPRSCAN
    
    Needs fixing!
    - how it works with the lookup table
    - top accessions based on AccNum + DA + Lineage
    - Read how it filters based on the Analysis with the most rows (won't always be Pfam, for instance)

--- R/ipr2viz.R
+++ R/ipr2viz.R
@@ -8,7 +8,11 @@ library(data.table)
 library(gggenes)
 library(ggplot2)
 #source("../the-approach/R/pre-msa-tree.R") # for "to_titlecase()"
+source("R/colnames_molevol.R")
 
+#################################
+## Modified gggenes::theme_genes
+#################################
 ## theme_genes2 adapted from theme_genes (w/o strip.text())
 ## https://github.com/wilkox/gggenes/blob/master/R/theme_genes.R
 theme_genes2 <- function() {
@@ -21,24 +25,34 @@ theme_genes2 <- function() {
     axis.ticks.y = ggplot2::element_blank(),
     axis.line.x = ggplot2::element_line(colour = "grey20", size = 0.5),
     axis.ticks.x = ggplot2::element_line(colour = "grey20", size = 0.5),
-    #strip.text = ggplot2::element_blank(),
     strip.background = ggplot2::element_blank()
+    #strip.text = ggplot2::element_blank()
   )
 }
 
+##################################
+## Get Top N AccNum by Lin+DomArch
+##################################
 # Group by lineage + DA then take top 20
-top_acc = function(cln_file, DA_col = "DomArch.Pfam", lin_col = "Lineage", n = 20)
+find_top_acc = function(infile_full,
+                        DA_col = "DomArch.Pfam", ## @SAM, you could pick by the Analysis w/ max rows!
+                        lin_col = "Lineage",
+                        n = 20)
 {
   lin_sym = sym(lin_col)
   DA_sym = sym(DA_col)
 
-  cln = fread(cln_file, sep ="\t", fill = T)
+  cln = fread(infile_full, sep ="\t", fill = T)
 
-  grouped = cln %>% group_by({{lin_sym}}, {{DA_sym}}) %>% summarise(count = n()) %>%
-    arrange(-count) %>% filter(!is.na({{lin_sym}}) & !is.na({{DA_sym}}))
+  ## Group by Lineage, DomArch and reverse sort by group counts
+  grouped = cln %>%
+    group_by({{lin_sym}}, {{DA_sym}}) %>%
+    summarise(count = n()) %>%
+    arrange(-count) %>%
+    filter(!is.na({{lin_sym}}) & !is.na({{DA_sym}}))
 
   top_acc = character(n)
-  for(r in 1:min(nrow(grouped),n))
+  for(r in 1:min(nrow(grouped), n))
   {
     l = (grouped %>% pull({{lin_sym}}))[r]
     DA = (grouped %>% pull({{DA_sym}}))[r]
@@ -54,72 +68,56 @@ top_acc = function(cln_file, DA_col = "DomArch.Pfam", lin_col = "Lineage", n = 2
 }
 
 
-
-
-
-
-ipr2viz <- function(infile_ipr=here("../molevolvr_out/phage1_out/WP_001901328.1_Vibrio_cholerae.iprscan.tsv"),
-                    cln_file = "../molevolvr_out/phage1_out/cln_combined.tsv",
-                    PfamClans_path = "TestData/Pfam-A.clans.txt",
-                    analysis=c("Pfam", "Phobius","TMHMM","SUPERFAMILY"),
-                    group_by = "DB",
-                    topn = 20, name = "AccNum", text_size = 10)
+#############################################
+## IPR + FULL files --> DomArch Visualization
+#############################################
+ipr2viz <- function(infile_ipr=NULL, infile_full=NULL,
+                    analysis=c("Pfam", "Phobius", "TMHMM", "Gene3D"),
+                    group_by = "Query", #"Analysis"
+                    topn = 20, name = "Name", text_size = 10)
 {
+  ## Read IPR file
+  ipr_out <- read_tsv(infile_ipr, col_names=T)
 
+  ## To filter by Analysis
   analysis = paste(analysis, collapse = "|")
 
-  colm <- c("SignAcc", "ClID", "Short_Name", "PFC_Name", "Description")
+  ## @SAM: This can't be set in stone since the analysis may change!
+  ## Getting top n accession numbers using find_top_acc()
+  top_acc <- find_top_acc(infile_full=infile_full,
+                          DA_col = "DomArch.Pfam",
+                          ## @SAM, you could pick by the Analysis w/ max rows!
+                          lin_col = "Lineage",
+                          n = topn)
 
-  PfamClans = read_tsv(PfamClans_path, col_names = colm)
+  # Filter by Top Accessions per Accession per DomArch and Lineage
+  ipr_out <- subset(ipr_out,
+                    ipr_out$AccNum %in% top_acc)
 
-  ## Read in Files and Assign ColNames
-  ipr_cols <- c("AccNum", "Seq_MD5_digest", "SeqLen", "Analysis",
-                "SignAcc", "SignDesc", "StartLoc", "StopLoc", "Score",
-                "Status", "RunDate",
-                "IPRAcc", "IPRDesc")#, "GOAnn", "PathAnn") ## OPTIONAL
-  ipr_out <- read_tsv(infile_ipr, col_names=ipr_cols, skip = 1) #%>%
-  #left_join(protmap, by="AccNum")
-
-  ## getting top n accession numbers
-  top_acc <- top_acc(cln_file, DA_col = "DomArch.Pfam", lin_col = "Lineage",
-                     n = topn)
-  # Add the lineages from the cln file
-  # lin_cols = c("AccNum", "Lineage", "Species")
-  ipr_out = merge(ipr_out, cln_file[, c("AccNum","Lineage", "Species")], by = "AccNum", all.x = T)
-
-
-  ipr_out <- subset(ipr_out, ipr_out$AccNum %in% top_acc)
-
-
-  #colnames(ipr_out) <- ipr_cols
+  ## Need to fix this eventually based on the 'real' gene orientation! :)
   ipr_out$Strand <- rep("forward", nrow(ipr_out))
 
-
-  #########ipr_out <- ipr_out %>% arrange(Leaf, StartLoc, StopLoc)
   ipr_out <- ipr_out %>% arrange(AccNum, StartLoc, StopLoc)
-  ipr_out_sub <- filter(ipr_out, grepl(pattern=analysis, x=Analysis))
-
+  ipr_out_sub <- filter(ipr_out,
+                        grepl(pattern=analysis, x=Analysis))
 
   # dynamic analysis labeller
-  analyses <- ipr_out_sub %>% select(Analysis) %>%
+  analyses <- ipr_out_sub %>%
+    select(Analysis) %>%
     distinct()
-  analysis_labeler <- analyses %>% pivot_wider(names_from = Analysis, values_from = Analysis)
+  analysis_labeler <- analyses %>%
+    pivot_wider(names_from = Analysis, values_from = Analysis)
   print(analysis_labeler)
 
   queryrows <- which(is.na(ipr_out_sub$AccNum))
 
-  ipr_out_sub <- left_join(ipr_out_sub, PfamClans, by = "SignAcc")
-
-  signacc <- which(is.na(ipr_out_sub$PFC_Name))
-  ipr_out_sub$PFC_Name[signacc] = ipr_out_sub$SignAcc[signacc]
-
   ## PLOTTING
   ## domains as separate arrows
-  if(group_by == "DB")
+  if(group_by == "Analysis")
   {
     ggplot(ipr_out_sub,
            aes_string(xmin = "StartLoc", xmax = "StopLoc",
-               y = name, fill = "SignDesc", label="PFC_Name")) +
+                      y = name, fill = "SignDesc", label="ShortName")) +
       geom_gene_arrow(arrowhead_height = unit(3, "mm"),
                       arrowhead_width = unit(1, "mm")) +
       geom_gene_label(align = "left") +
@@ -129,18 +127,20 @@ ipr2viz <- function(infile_ipr=here("../molevolvr_out/phage1_out/WP_001901328.1_
       #, ncol = 1 + #scales = "free",
       scale_fill_brewer(palette = "Set3") +
       theme_minimal() + theme_genes2() +
-      theme(legend.position="bottom", legend.box = "horizontal",
-            legend.key.size = unit(0.02, "npc"), legend.box.margin = margin(),
+      theme(legend.position="bottom",
+            legend.box = "horizontal",
+            legend.key.size = unit(0.02, "npc"),
+            legend.box.margin = margin(),
             text = element_text(size = text_size)) +
       ylab("")+
       guides(fill=guide_legend(nrow=3))
   }
 
-  else if(group_by == "protein"){
+  else if(group_by == "Query"){
     ggplot(ipr_out_sub,
            aes(xmin = StartLoc, xmax = StopLoc,
-               y = Analysis  #y = AccNum
-               , fill = SignDesc, label=PFC_Name)) +
+               y = Analysis,  #y = AccNum
+               fill = SignDesc, label = ShortName)) +
       geom_gene_arrow(arrowhead_height = unit(3, "mm"),
                       arrowhead_width = unit(1, "mm")) +
       geom_gene_label(align = "left") +
@@ -149,8 +149,10 @@ ipr2viz <- function(infile_ipr=here("../molevolvr_out/phage1_out/WP_001901328.1_
                  labeller=as_labeller(analysis_labeler)) +
       scale_fill_brewer(palette = "Set3") +
       theme_minimal() + theme_genes2() +
-      theme(legend.position="bottom", legend.box = "horizontal",
-            legend.key.size = unit(0.02, "npc"), legend.box.margin = margin(),
+      theme(legend.position="bottom",
+            legend.box = "horizontal",
+            legend.key.size = unit(0.02, "npc"),
+            legend.box.margin = margin(),
             text = element_text(size = text_size)) +
       ylab("")+
       guides(fill=guide_legend(nrow=3))
@@ -158,43 +160,37 @@ ipr2viz <- function(infile_ipr=here("../molevolvr_out/phage1_out/WP_001901328.1_
 
 }
 
-ipr2viz_noblast <- function(infile_ipr,
-                    accessions,
-                    PfamClans_path = "TestData/Pfam-A.clans.txt",
-                    analysis=c("Pfam", "Phobius","TMHMM","SUPERFAMILY"),
-                    group_by = "DB", name = "AccNum", text_size = 10)
+ipr2viz_web <- function(infile_ipr,
+                        accessions,
+                        analysis=c("Pfam", "Phobius","TMHMM","Gene3D"),
+                        group_by = "Analysis", name = "AccNum",
+                        text_size = 10)
 {
-
+  ## To filter by Analysis
   analysis = paste(analysis, collapse = "|")
 
-  colm <- c("SignAcc", "ClID", "Short_Name", "PFC_Name", "Description")
+  ## @SAM, colnames, merges, everything neeeds to be done now based on the
+  ## combined lookup table from "common_data"
+  lookup_tbl_path = "/data/research/jravilab/common_data/lookup_tbl.tsv"
+  lookup_tbl = read_tsv(lookup_tbl_path, col_names = T)
 
-  PfamClans = read_tsv(PfamClans_path, col_names = colm)
-
-  ## Read in Files and Assign ColNames
-  # ipr_cols <- c("AccNum", "Seq_MD5_digest", "SeqLen", "Analysis",
-  #               "SignAcc", "SignDesc", "StartLoc", "StopLoc", "Score",
-  #               "Status", "RunDate",
-  #               "IPRAcc", "IPRDesc")#, "GOAnn", "PathAnn") ## OPTIONAL
-  ipr_out <- fread(infile_ipr, fill = T, sep = "\t")
-  #left_join(protmap, by="AccNum")
-
+  ## Read IPR file and subset by Accessions
+  ipr_out <- read_tsv(infile_ipr, col_names=ipr_colnames)
   ipr_out <- subset(ipr_out, ipr_out$AccNum %in% accessions)
 
-
-  #colnames(ipr_out) <- ipr_cols
+  ## Need to fix eventually based on 'real' gene orientation!
   ipr_out$Strand <- rep("forward", nrow(ipr_out))
 
-
-  #########ipr_out <- ipr_out %>% arrange(Leaf, StartLoc, StopLoc)
   ipr_out <- ipr_out %>% arrange(AccNum, StartLoc, StopLoc)
-  ipr_out_sub <- filter(ipr_out, grepl(pattern=analysis, x=Analysis))
+  ipr_out_sub <- filter(ipr_out,
+                        grepl(pattern=analysis, x=Analysis))
 
   # dynamic analysis labeller
   analyses <- ipr_out_sub %>% select(Analysis) %>%
     distinct()
-  analysis_labeler <- analyses %>% mutate(id = 1) %>% pivot_wider(names_from = Analysis,
-                                                             values_from = Analysis) %>%
+  analysis_labeler <- analyses %>% mutate(id = 1) %>%
+    pivot_wider(names_from = Analysis,
+                values_from = Analysis) %>%
     select(-id)
   # analysis_labeler[1,] = colnames(analysis_labeler)
 
@@ -202,20 +198,22 @@ ipr2viz_noblast <- function(infile_ipr,
 
   queryrows <- which(is.na(ipr_out_sub$AccNum))
 
+  ## @SAM, make sure the following two work with the Lookup Tables!!
   ipr_out_sub = dplyr::rename(ipr_out_sub,  "SignAcc" = "DB.ID")
-  ipr_out_sub <- left_join(ipr_out_sub, PfamClans, by = "SignAcc")
+  ipr_out_sub <- merge(ipr_out_sub, lookup_tbl, by = "SignAcc")
 
-  signacc <- which(is.na(ipr_out_sub$PFC_Name))
-  ipr_out_sub$PFC_Name[signacc] = ipr_out_sub$SignAcc[signacc]
+  signacc <- which(is.na(ipr_out_sub$ShortName))
+  ipr_out_sub$ShortName[signacc] = ipr_out_sub$SignAcc[signacc]
 
   name_sym = sym(name)
+
   ## PLOTTING
   ## domains as separate arrows
-  if(group_by == "DB")
+  if(group_by == "Analysis")
   {
     ggplot(ipr_out_sub,
            aes_string(xmin = "StartLoc", xmax = "StopLoc",
-               y = name, fill = "SignDesc", label="PFC_Name")) +
+                      y = name, fill = "SignDesc", label="ShortName")) +
       geom_gene_arrow(arrowhead_height = unit(3, "mm"),
                       arrowhead_width = unit(1, "mm")) +
       geom_gene_label(align = "left") +
@@ -225,28 +223,33 @@ ipr2viz_noblast <- function(infile_ipr,
       #, ncol = 1 + #scales = "free",
       scale_fill_brewer(palette = "Set3") +
       theme_minimal() + theme_genes2() +
-      theme(legend.position="bottom", legend.box = "horizontal",
-            legend.key.size = unit(0.02, "npc"), legend.box.margin = margin(),
+      theme(legend.position="bottom",
+            legend.box = "horizontal",
+            legend.key.size = unit(0.02, "npc"),
+            legend.box.margin = margin(),
             text = element_text(size = text_size)) +
       ylab("")+
       guides(fill=guide_legend(nrow=3))
   }
 
-  else if(group_by == "protein"){
+  else if(group_by == "Query"){
     ggplot(ipr_out_sub,
            aes(xmin = StartLoc, xmax = StopLoc,
-               y = Analysis  #y = AccNum
-               , fill = SignDesc, label=PFC_Name)) +
+               y = Analysis,  #y = AccNum
+               fill = SignDesc, label=ShortName)) +
       geom_gene_arrow(arrowhead_height = unit(3, "mm"),
                       arrowhead_width = unit(1, "mm")) +
       geom_gene_label(align = "left") +
 
-      facet_wrap(as.formula(paste("~", name)), strip.position = "top", ncol = 3,
+      facet_wrap(as.formula(paste("~", name)),
+                 strip.position = "top", ncol = 3,
                  labeller=as_labeller(analysis_labeler)) +
       scale_fill_brewer(palette = "Set3") +
       theme_minimal() + theme_genes2() +
-      theme(legend.position="bottom", legend.box = "horizontal",
-            legend.key.size = unit(0.02, "npc"), legend.box.margin = margin(),
+      theme(legend.position="bottom",
+            legend.box = "horizontal",
+            legend.key.size = unit(0.02, "npc"),
+            legend.box.margin = margin(),
             text = element_text(size = text_size)) +
       ylab("")+
       guides(fill=guide_legend(nrow=3))
