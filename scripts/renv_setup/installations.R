8030f0b5dc59dfe4a5c2b5d9a028d233fa81fa82
Janani Ravi <janani@msu.edu>
2021-01-12 16:29:06 -0500

    Update installations.R
    - added a few extra packages needed by our scripts
    - reorganized based on groups/kinds of packages
    - installations work with R v4.0.3 (occassionally with RStudio)
    @samuelzornchen @MaryZhang8 @laurensosinski

--- scripts/renv_setup/installations.R
+++ scripts/renv_setup/installations.R
@@ -1,24 +1,47 @@
 
 # CRAN installations
-install.packages(c("devtools", "conflicted","docstring", "roxygen2",
-                   "tidyverse", "here", "testthat", "reprex",
-                   "rmarkdown", "knitr", "blogdown", "bookdown",
-                   "Rcpp", "rgl",
-                   "gridExtra", "cowplot", "gganimate", "plotly",
-                   "ggthemes", "ggsci",
-                   "UpSetR", "wordcloud2", "igraph", "tidytext",
-                   "ape", "phylogram", "seqinr", "phylotools", "phytools",
-                   "tidytree"), dependencies=T)
+install.packages(c('devtools', "usethis", "testthat", "reprex",
+                   "docstring", "roxygen2"),
+                 dependencies=T)
 
-install.packages("BiocManager", dependencies=T)
-# trouble with OpenMP
-# install.packages("data.table", dependencies=T)
+install.packages(c("conflicted", "tidyverse", "here",
+                   "rmarkdown", "knitr", "xtable", "DT", "gt",
+                   "blogdown", "bookdown", "pkgdown",
+                   "shiny", "shinydashboard", "flexdashboard"),
+                 dependencies=T)
+
+install.packages(c("Rcpp", "data.table", "rlang", "glue",
+                   "future", "furrr",
+                   "remotes", "rgl",
+                   "htmlwidgets", "gh", "httr", "XML",
+                   "drat", "rhub", "covr",
+                   "RPushbullet", "jsonlite"),
+                 dependencies=T)
+
+install.packages(c("gridExtra", "cowplot", "gganimate", "plotly", "tidytext",
+                   "ggvis", "ggthemes", "ggsci", "gggenes",
+                   "UpSetR", "wordcloud2", "wordcloud", "sunburstR",
+                   "igraph", "ggraph", "visNetwork", #"network3D", "d3Network",
+                   "heatmap3", "heatmaply", "viridis", "d3r",
+                   "rentrez", "reutils", "biomartr",
+                   "ape", "phylogram", "phangorn", "seqinr",
+                   "phylotools", "phytools", "tidytree",
+                   "pdftools", "latexpdf", "tinytex"),
+                 dependencies=T)
 
 # Bioconductor installations
 if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+    install.packages("BiocManager", dependencies=T)
 BiocManager::install("msa")
-# BiocManager::install("ggtree") # ERROR w/ fopenmp
+BiocManager::install("Biostrings")
 
 # GitHub and Devtools installations
+devtools::install_github("rstudio/d3heatmap")
+remotes::install_github("mhahsler/rMSA")
 devtools::install_github("GuangchuangYu/ggtree")
+
+# Other
+remotes::install_github("ropenscilabs/travis")
+
+# trouble with OpenMP
+# BiocManager::install("ggtree") # ERROR w/ fopenmp
