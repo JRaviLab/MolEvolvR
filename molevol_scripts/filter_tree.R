cf5479bb78759db4e05f18f1098f9f963461dc3b
Samuel Chen <chensam2@msu.edu>
2021-01-14 20:57:22 -0500

    Add rename_fasta function

--- molevol_scripts/filter_tree.R
+++ molevol_scripts/filter_tree.R
@@ -9,49 +9,49 @@ source(here("R/combine_analysis.R"))
 source(here('molevol_scripts/ipr2viz_copy.R'))
 
 
-## READ FILE
-inpath="../molevol_data/project_data/phage_defense/full_analysis_20210108/"
-
-cln_combnd <- read_tsv(paste0(inpath, "cln_combined.tsv", collapse=""),
-                       col_names = T) %>%
-  mutate(Genus=word(Species, start=1L))
-
-# cln <- fread(infile_full, sep ="\t", fill = T)
-
-## Filter by DA of interest
-table(cln_combnd$DomArch.Gene3D) %>%
-  sort(decreasing = T)
-g3d_ofinterest <- c("P-loop_containing_nucleotide_triphosphate_hydrolases",
-                    "Cytidine_Deaminase_domain_2")
-
-
-## Group by Lineage, Genus, and DomArch | reverse sort by PcPositive
-col1 <- "Lineage"; sym1 <- sym(col1)
-col2 <- "Genus"; sym2 <- sym(col2)
-col3 <- "DomArch.Pfam"; sym3 <- sym(col3)
-
-cln <- cln_combnd %>%
-  filter(grepl(g3d_ofinterest[2], DomArch.Gene3D)) %>%
-  filter(grepl(g3d_ofinterest[1], DomArch.Gene3D)) %>%
-  select(AccNum, Name, ProteinName,
-         Query, PcPositive,
-         TaxID, Species, Genus, Lineage,
-         SourceDB, Completeness,
-         starts_with("DomArch")) %>%
-  group_by({{sym1}}, {{sym2}}, {{sym3}}) %>%
-  arrange(-PcPositive) %>%
-  slice_head(n=1) %>%
-  filter(!is.na({{sym1}}) & !is.na({{sym2}}) & !is.na({{sym3}}))
-
-table(cln$DomArch.Gene3D) %>%
-  sort(decreasing = T)
-
-accnum_list <- cln$AccNum %>% as.character()
-write_lines(x=accnum_list,
-          path=paste0(inpath, "g3d_both_accnum.txt", collapse=""))
+# ## READ FILE
+# inpath="../molevol_data/project_data/phage_defense/full_analysis_20210108/"
+#
+# cln_combnd <- read_tsv(paste0(inpath, "cln_combined.tsv", collapse=""),
+#                        col_names = T) %>%
+#   mutate(Genus=word(Species, start=1L))
+#
+# # cln <- fread(infile_full, sep ="\t", fill = T)
+#
+# ## Filter by DA of interest
+# table(cln_combnd$DomArch.Gene3D) %>%
+#   sort(decreasing = T)
+# g3d_ofinterest <- c("P-loop_containing_nucleotide_triphosphate_hydrolases",
+#                     "Cytidine_Deaminase_domain_2")
+#
+#
+# ## Group by Lineage, Genus, and DomArch | reverse sort by PcPositive
+# col1 <- "Lineage"; sym1 <- sym(col1)
+# col2 <- "Genus"; sym2 <- sym(col2)
+# col3 <- "DomArch.Pfam"; sym3 <- sym(col3)
+#
+# cln <- cln_combnd %>%
+#   filter(grepl(g3d_ofinterest[2], DomArch.Gene3D)) %>%
+#   filter(grepl(g3d_ofinterest[1], DomArch.Gene3D)) %>%
+#   select(AccNum, Name, ProteinName,
+#          Query, PcPositive,
+#          TaxID, Species, Genus, Lineage,
+#          SourceDB, Completeness,
+#          starts_with("DomArch")) %>%
+#   group_by({{sym1}}, {{sym2}}, {{sym3}}) %>%
+#   arrange(-PcPositive) %>%
+#   slice_head(n=1) %>%
+#   filter(!is.na({{sym1}}) & !is.na({{sym2}}) & !is.na({{sym3}}))
+#
+# table(cln$DomArch.Gene3D) %>%
+#   sort(decreasing = T)
+#
+# accnum_list <- cln$AccNum %>% as.character()
+# write_lines(x=accnum_list,
+#           path=paste0(inpath, "g3d_both_accnum.txt", collapse=""))
 
 filter_tree = function(cln_comined_path,ppos_threshold = 30,
-                       subset_col = "DomArch.pfam", domains_of_interest = c(),
+                       subset_col = "DomArch.Pfam", domains_of_interest = c(),
                        interest_col = NULL, tail_cutoff = 2){
   cln_combnd <- read_tsv(cln_comined_path,
                          col_names = T) %>%
@@ -100,20 +100,27 @@ filter_tree = function(cln_comined_path,ppos_threshold = 30,
 }
 
 
-acc = filter_tree("../molevol_data/project_data/phage_defense/full_analysis_20210108/cln_combined.tsv",
-            domains_of_interest =  c("P-loop_containing_nucleotide_triphosphate_hydrolases",
-                                     "Cytidine_Deaminase_domain_2"),
-            interest_col = "DomArch.Gene3D", tail_cutoff = 5)
-source("R/pre-msa-tree.R")
-tmp_fa = tempfile()
-acc2fa(acc, tmp_fa)
-
-tmp_msa = tempfile()
-
-alignFasta(tmp_fa, tool = "ClustalOmega" ,outpath = tmp_msa )
-
-source("scripts/tree.R")
-tree = seq_tree(tmp_msa)
+# acc = filter_tree("../molevol_data/project_data/phage_defense/full_analysis_20210108/cln_combined.tsv",
+#             domains_of_interest =  c("P-loop_containing_nucleotide_triphosphate_hydrolases",
+#                                      "Cytidine_Deaminase_domain_2"),
+#             interest_col = "DomArch.Gene3D", tail_cutoff = 5)
+#
+# cln_combined = fread("../molevol_data/project_data/phage_defense/full_analysis_20210108/cln_combined.tsv",
+#                      sep = "\t", fill = T)
+# source("R/pre-msa-tree.R")
+# tmp_fa = tempfile()
+# acc2fa(acc, tmp_fa)
+#
+# renamed = rename_fasta(fa_path = "../molevolvr_app/TestData/test_6q.fa",
+#                    replacement_function = map_acc2name, acc2name = cln_combined)
+#
+#
+# tmp_msa = tempfile()
+#
+# alignFasta(tmp_fa, tool = "ClustalOmega" ,outpath = tmp_msa )
+#
+# source("scripts/tree.R")
+# tree = seq_tree(tmp_msa)
 
 
 # write(read_file(tmp_msa), "../molevol_data/project_data/phage_defense/full_analysis_20210108/pfam_subset_msa.msa")
