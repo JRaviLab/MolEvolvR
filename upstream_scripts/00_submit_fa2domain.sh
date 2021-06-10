98a7c49c95a863b1fe7ae3e5f0d4728d2cdb61f6
jburke11 <burkej24@msu.edu>
2021-06-10 14:07:15 -0400

    fixes for domain submission script

--- upstream_scripts/00_submit_fa2domain.sh
+++ upstream_scripts/00_submit_fa2domain.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-
+#PBS -N submit_fa2domain
+#PBS -l nodes=1 :ppn 8
 ## COMPANION SCRIPT TO MOLEVOLVR APP ##
 ## Authors: Joe Burke
 
 ## USER INPUTS
-INFILE=$1
-SCRIPT=$2
-OTHERARGS=$3
+echo $INFILE
+echo $SCRIPT
+echo $OTHERARGS
 DIR=$(dirname $INFILE)
 BASE=$(basename $INFILE)
 PREFIX=$(echo "${BASE%%.*}")
 cd ${DIR}
+module load iprscan
+module load R
 
 sh /data/research/jravilab/molevol_scripts/upstream_scripts/04a_iprscan.sh $INFILE ${PREFIX} $DIR
 
-Rscript --vanilla fa2domain.R $INFILE ${PREFIX}.iprscan domains.fa
+Rscript --vanilla /data/research/jravilab/molevol_scripts/upstream_scripts/fa2domain.R $INFILE ${PREFIX}.iprscan ${DIR}/domains.fa
 
-qsub ${SCRIPT} domains.fa ${OTHERARGS}
+sh ${SCRIPT} domains.fa ${OTHERARGS}
