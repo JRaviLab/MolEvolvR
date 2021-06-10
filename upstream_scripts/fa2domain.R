62983c6e09dd3a3569c3d0eedecc179e0befdc8b
jburke11 <burkej24@msu.edu>
2021-06-10 15:45:11 -0400

    working fa2domain scripts

--- upstream_scripts/fa2domain.R
+++ upstream_scripts/fa2domain.R
@@ -9,7 +9,7 @@ for (item in names(in_fa)){
   for (i in 1:nrow(in_ipr)){
     if (in_ipr[i, 1] == accession){
       sequence = toString(subseq(in_fa[item], in_ipr[i, 7], in_ipr[i, 8]))
-      header <- paste0("<",accession, " ", in_ipr[i,5], " ", in_ipr[i,6] )
+      header <- paste0(">",accession, "-", i, " ", in_ipr[i,5], " ", in_ipr[i,6] )
       complete_seq = paste(header, sequence, sep="\n")
       cat(complete_seq, file=out_file, append=TRUE, sep = "\n")
     }
