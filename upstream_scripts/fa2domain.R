eab536578324b76b1d1000fa174002a6db089bad
jburke11 <burkej24@msu.edu>
2021-08-23 11:10:02 -0400

    edit split domain names and add check for analysis completion

--- upstream_scripts/fa2domain.R
+++ upstream_scripts/fa2domain.R
@@ -9,7 +9,7 @@ for (item in names(in_fa)){
   for (i in 1:nrow(in_ipr)){
     if (in_ipr[i, 1] == accession && (in_ipr[i,4] == "Pfam" || in_ipr[i,4] == "Gene3D")){
       sequence = toString(subseq(in_fa[item], in_ipr[i, 7], in_ipr[i, 8]))
-      header <- paste0(">",accession, "-", i, " ", in_ipr[i,5], " ", in_ipr[i,6] )
+      header <- paste0(">",accession, "_", in_ipr[i,4], "-", i, " ", in_ipr[i,5], " ", in_ipr[i,6] )
       complete_seq = paste(header, sequence, sep="\n")
       cat(complete_seq, file=out_file, append=TRUE, sep = "\n")
     }
