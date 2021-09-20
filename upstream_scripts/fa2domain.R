12eec0dc9ab06f83e7dc1954c4090d4ede0e2ff2
jburke11 <burkej24@msu.edu>
2021-09-20 15:56:45 -0400

    add more accurate domain count

--- upstream_scripts/fa2domain.R
+++ upstream_scripts/fa2domain.R
@@ -7,11 +7,13 @@ for (item in names(in_fa)){
   accession = unlist(strsplit(item, " "))[1]
   print(accession)
   for (i in 1:nrow(in_ipr)){
+    domain_count = 0
     if (in_ipr[i, 1] == accession && (in_ipr[i,4] == "Pfam" || in_ipr[i,4] == "Gene3D")){
       sequence = toString(subseq(in_fa[item], in_ipr[i, 7], in_ipr[i, 8]))
-      header <- paste0(">",accession, "_", in_ipr[i,4], "-", i, " ", in_ipr[i,5], " ", in_ipr[i,6] )
+      header <- paste0(">",accession, "_", in_ipr[i,4], "-", domain_count, " ", in_ipr[i,5], " ", in_ipr[i,6] )
       complete_seq = paste(header, sequence, sep="\n")
       cat(complete_seq, file=out_file, append=TRUE, sep = "\n")
+      domain_count <- domain_count + 1
     }
     else{
     }
