a92cf74ec839f6ceaf43d78bbaafc6b1be40494c
jburke11 <burkej24@msu.edu>
2022-05-18 16:16:49 -0400

    add backend updates

--- upstream_scripts/fa2domain.R
+++ upstream_scripts/fa2domain.R
@@ -1,14 +1,16 @@
 library("Biostrings")
+source("/data/research/jravilab/molevol_scripts/upstream_scripts/00_submit_full.R")
 args = commandArgs(trailingOnly = TRUE)
 in_fa = readAAStringSet(args[1])
 in_ipr = read.csv(args[2], sep="\t", header=FALSE)
 out_file = args[3]
 for (item in names(in_fa)){
   accession = unlist(strsplit(item, " "))[1]
+  write(accession, "starting_accs.txt", append = TRUE)
   pfam_count = 1
   gene3d_count =1
   for (i in 1:nrow(in_ipr)){
-    if (in_ipr[i, 1] == accession && (in_ipr[i,4] == "Pfam")){
+    if (in_ipr[i, 1] == accession  && (in_ipr[i,4] == "Pfam")){
       sequence = toString(subseq(in_fa[item], in_ipr[i, 7], in_ipr[i, 8]))
       header <- paste0(">",accession, "_", in_ipr[i,4], "-", pfam_count, " ", in_ipr[i,5], " ", in_ipr[i,6] )
       complete_seq = paste(header, sequence, sep="\n")
@@ -26,3 +28,5 @@ for (item in names(in_fa)){
     }
   }
 }
+
+submit_full(dir = args[4], sequences = out_file, phylo = args[5], by_domain = TRUE, domain_starting = args[1])
