4dc569936fc53ffb6c7ebe5b97fb26cb668c5ca2
Janani Ravi <janani@msu.edu>
2023-02-28 12:50:43 -0700

    mv to psp_app repo
    Co-Authored-By: Samuel Chen <47834379+samuelzornchen@users.noreply.github.com>

--- psp-shiny/navbar-title-hyperlink.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  $(".navbar .container-fluid .navbar-header").prepend('<a href = "https://jravilab.shinyapps.io/psp-evolution/">');
-  $(".navbar .container-fluid .navbar-header").append('<\a>');
-});
