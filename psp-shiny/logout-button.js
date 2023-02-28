615ad27b14bc9cb32dca483319c49b2faecc6253
Janani Ravi <janani@msu.edu>
2023-02-28 12:50:43 -0700

    mv to psp_app repo
    Co-Authored-By: Samuel Chen <47834379+samuelzornchen@users.noreply.github.com>

--- psp-shiny/logout-button.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $(".navbar .container-fluid").append('<div class="pull-right"><button id="logout-button" class="btn navbar-btn action-button btn-danger shiny-bound-input" style="color: white; display: none;" type="button">Log out</button></div>');
-});
