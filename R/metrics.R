6c4422629499f9eff7d673477d092bf18fc8c389
jakekrol <jdkrol351@gmail.com>
2023-12-05 00:48:05 -0700

    R: minor tweaks/fixes to log-metric funcs

--- R/metrics.R
+++ R/metrics.R
@@ -4,9 +4,10 @@
 
 #' Stack rows of job logfiles
 #' @param dir_job_results the base directory containing all job_results folders as subdirs
+#' @return list of log table and a character vector of logfile paths that failed to read
 # example
 #   path_dev_results <- "/data/molevolvr_transfer/molevolvr_dev/job_results"
-#   df_logs <- aggregate_logs(path_dev_results, verbose = TRUE)
+#   logs <- aggregate_logs(path_dev_results, verbose = TRUE)
 aggregate_logs <- function(dir_job_results, verbose = FALSE) {
   vec_dir_results <- list.dirs(
     dir_job_results,
@@ -22,6 +23,8 @@ aggregate_logs <- function(dir_job_results, verbose = FALSE) {
   df_log <- tibble::tibble()
   # logfiles that could not be read (or they were never written)
   failed_reads <- c()
+  # empty logsfiles
+  empty_logs <- c()
   for (dir in vec_dir_results) {
     tryCatch(
       expr = {
@@ -30,12 +33,12 @@ aggregate_logs <- function(dir_job_results, verbose = FALSE) {
           show_col_types = F
         )
         df_log <- df_log |> dplyr::bind_rows(df)
+        if (nrow(df_log) == 0L) {
+          empty_logs <- append(x = empty_logs, values = dir)
+        }
       },
       error = function(e) {
-        failed_reads <- append(
-          failed_reads,
-          dir
-        )
+        failed_reads <- append(x = failed_reads, values = dir)
         if (verbose) {
           stringr::str_glue("failed to read {file.path(dir, 'logfile.tsv')}") |>
             warning()
@@ -45,7 +48,13 @@ aggregate_logs <- function(dir_job_results, verbose = FALSE) {
   }
   # reset warning option
   if (verbose) {options(warn = opt_warn)}
-  return(list("df_log" = df_log, "failed_reads" = failed_reads))
+  return(
+    list(
+      "df_log" = df_log,
+      "failed_reads" = failed_reads,
+      "empty_logs" = empty_logs
+    )
+  )
 }
 
 #' calc statistic for processes in MolEvolvR logfiles
@@ -55,13 +64,14 @@ aggregate_logs <- function(dir_job_results, verbose = FALSE) {
 #' @metric a function to apply on the column(s)
 #' @return if group_by is empty: a single value; else: tibble from summarise
 # example
+# median (no row grouping)
 #   path_dev_results <- "/data/molevolvr_transfer/molevolvr_dev/job_results"
-#   df_log <- aggregate_logs(path_dev_results)
-#   result <- df_log$df_log |> calc_log_process_stat("dblast", f = mean)
-# example group_by
+#   logs <- aggregate_logs(path_dev_results)
+#   dblast_median <- logs$df_log |> calc_log_process_stat(columns = 'dblast', f = median)
+# median (group_by query)
 #   path_dev_results <- "/data/molevolvr_transfer/molevolvr_dev/job_results"
-#   df_log <- aggregate_logs(path_dev_results)
-#   df_log$df_log |> count_log_process_stat(columns = 'dblast', columns_group_by = 'query')
+#   logs <- aggregate_logs(path_dev_results)
+#   dblast_median_by_query <- logs$df_log |> calc_log_process_stat(columns = 'dblast', f = median, columns_group_by = 'query')
 calc_log_process_stat <- function(
   df_log, columns, f, ..., columns_group_by = character(0)
 ) {
@@ -69,7 +79,7 @@ calc_log_process_stat <- function(
   if (purrr::is_empty(columns_group_by)) {
     result <- df_log |>
       tidyr::drop_na(columns) |>
-      dplyr::mutate('stat' = dplyr::across(columns,  f)) |>
+      dplyr::mutate('stat' = dplyr::across(columns,  f, ...)) |>
       dplyr::pull('stat') |>
       dplyr::pull() |>
       unique()
@@ -77,9 +87,9 @@ calc_log_process_stat <- function(
     # group rows
     result <- df_log |>
       tidyr::drop_na(columns) |>
-      dplyr::group_by(columns_group_by) |>
-      dplyr::summarise('stat' = f)
+      dplyr::group_by(.data[[columns_group_by]]) |>
+      dplyr::mutate('stat' = dplyr::across(columns, f, ...)) |>
+      dplyr::ungroup()
   }
   return(result)
 }
-
