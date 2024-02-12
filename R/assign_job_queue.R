eb886b57bafb6a4e91ad28665a716f87c80eb346
jakekrol <jdkrol351@gmail.com>
2024-02-12 15:10:03 -0700

    cleanup code before merge:
    - Document functions
    - Simplify code
    - Apply suggestions from PR thread

--- R/assign_job_queue.R
+++ R/assign_job_queue.R
@@ -4,10 +4,9 @@
 # to use this, construct paths like so: file.path(common_root, "path", "to", "file.R")
 # for example, the reference for this file would be:
 # file.path(common_root, "molevol_scripts", "R", "assign_job_queue.R")
-common_root <- Sys.getenv('COMMON_SRC_ROOT')
+common_root <- Sys.getenv("COMMON_SRC_ROOT")
 
 #' Construct list where names (MolEvolvR advanced options) point to processes
-#'
 #'
 #' @return list where names (MolEvolvR advanced options) point to processes
 #'
@@ -16,7 +15,7 @@ make_opts2procs <- function() {
   opts2processes <- list(
     "homology_search" = c("dblast", "dblast_cleanup"),
     "domain_architecture" = c("iprscan", "ipr2lineage", "ipr2da"),
-    "any" = c("blast_clust", "clust2table") # processes always present agnostic of advanced options
+    "always" = c("blast_clust", "clust2table") # processes always present agnostic of advanced options
   )
   return(opts2processes)
 }
@@ -32,8 +31,8 @@ make_opts2procs <- function() {
 #' advanced_opts <- c("homology_search", "domain_architecture")
 #' procs <- map_advanced_opts2procs(advanced_opts)
 map_advanced_opts2procs <- function(advanced_opts) {
-  # append 'any' to add procs that always run
-  advanced_opts <- c(advanced_opts, "any")
+  # append 'always' to add procs that always run
+  advanced_opts <- c(advanced_opts, "always")
   opts2proc <- make_opts2procs()
   # setup index for opts2proc based on advanced options
   idx <- which(names(opts2proc) %in% advanced_opts)
@@ -42,83 +41,82 @@ map_advanced_opts2procs <- function(advanced_opts) {
   return(procs)
 }
 
-#' Scrape logs and calculate median processes
+#' Scrape MolEvolvR logs and calculate median processes
 #'
-#' for now execute this code outside of container
+#' @param dir_job_results [chr] path to MolEvolvR job_results 
+#' directory
 #'
-#' @return list: names are processes and point to a single value of 
-#' median seconds for a given process
+#' @return [list] names: processes; values: median runtime (seconds)
 #'
 #' see molevol_scripts/R/metrics.R for info on functions called here
 #'
-#' example:
-#' advanced_opts <- c("homology_search", "domain_architecture")
-#' procs <- map_advanced_opts2procs(advanced_opts)
-get_proc_medians <- function(job_results_folder) {
+#' examples:
+#' 
+#' 1)
+#' dir_job_results <- "/data/scratch/janani/molevolvr_out"
+#' list_proc_medians <- get_proc_medians(dir_job_results)
+#' 
+#' 2) from outside container environment
+#' common_root <- "/data/molevolvr_transfer/molevolvr_dev"
+#' dir_job_results <- "/data/molevolvr_transfer/molevolvr_dev/job_results"
+#' list_proc_medians <- get_proc_medians(dir_job_results)
+#'
+get_proc_medians <- function(dir_job_results) {
   source(file.path(common_root, "molevol_scripts", "R", "metrics.R"))
   
   # aggregate logs from
-  path_prod_results <- job_results_folder
   path_log_data <- file.path(common_root, "molevol_scripts", "log_data", "prod_logs.rda")
 
   # ensure the folder exists to the location
-  dir.create(dirname(path_log_data), recursive=TRUE)
+  if (!dir.exists(path_log_data)) {
+    dir.create(dirname(path_log_data), recursive = TRUE, showWarnings = FALSE)
+  }
 
+  # attempt to load pre-generated logdata
   if (!file.exists(path_log_data)) {
-    logs <- aggregate_logs(path_prod_results, latest_date = Sys.Date() - 60)
+    logs <- aggregate_logs(dir_job_results, latest_date = Sys.Date() - 60)
     save(logs, file = path_log_data)
   } else {
     load(path_log_data) # loads the logs object
   }
   df_log <- logs$df_log
-
-  dblast_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'dblast', f = median)
-  dblast_cleanup_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'dblast_cleanup', f = median)
-  iprscan_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'iprscan', f = median)
-  ipr2lineage_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'ipr2lineage', f = median)
-  ipr2da_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'ipr2da', f = median)
-  blast_clust_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'blast_clust', f = median)
-  clust2table_median <- df_log |>
-    calc_log_process_stat(na.rm = T, columns = 'clust2table', f = median)
-  return(
-    list(
-      "dblast" = dblast_median,
-      "dblast_cleanup" = dblast_cleanup_median,
-      "iprscan" = iprscan_median,
-      "ipr2lineage" = ipr2lineage_median,
-      "ipr2da" = ipr2da_median,
-      "blast_clust" = blast_clust_median,
-      "clust2table" = clust2table_median
-    )
+  procs <- c(
+    "dblast", "dblast_cleanup", "iprscan",
+    "ipr2lineage", "ipr2da", "blast_clust",
+    "clust2table"
   )
+  list_proc_medians <- df_log |>
+    dplyr::select(dplyr::all_of(procs)) |>
+    dplyr::summarise(
+      dplyr::across(
+        dplyr::everything(),
+        \(x) median(x, na.rm = TRUE)
+      )
+    ) |>
+    as.list()
+  return(list_proc_medians)
 }
 
 #' Write a table of 2 columns: 1) process and 2) median seconds
 #'
-#' @param filepath path to save table
+#' @param dir_job_results [chr] path to MolEvolvR job_results
+#' @param filepath path to save tsv file
 #'
-#' @return tibble: 2 columns: 1) process and 2) median seconds
+#' @return [tbl_df] 2 columns: 1) process and 2) median seconds
 #'
 #' example: write_proc_medians_table(
 #'   "/data/scratch/janani/molevolvr_out/",
 #'   "/data/scratch/janani/molevolvr_out/log_tbl.tsv"
 #' )
-write_proc_medians_table <- function(job_results_folder, filepath) {
-proc_medians <- get_proc_medians(job_results_folder)
-df_proc_medians <- proc_medians |>
-  tibble::as_tibble() |>
-  tidyr::pivot_longer(
-    dplyr::everything(),
-    names_to = "process",
-    values_to = "median_seconds"
-  ) |>
-  dplyr::arrange(dplyr::desc(median_seconds))
+write_proc_medians_table <- function(dir_job_results, filepath) {
+  df_proc_medians <- get_proc_medians(dir_job_results) |>
+    tibble::as_tibble() |>
+    tidyr::pivot_longer(
+      dplyr::everything(),
+      names_to = "process",
+      values_to = "median_seconds"
+    ) |>
+    dplyr::arrange(dplyr::desc(median_seconds))
   readr::write_tsv(df_proc_medians, file = filepath)
   return(df_proc_medians)
 }
@@ -130,24 +128,30 @@ df_proc_medians <- proc_medians |>
 #' MOLEVOLVR_PROC_WEIGHTS, which get_proc_weights() also uses as its default
 #' read location.
 #'
-#' @param filepath path to save YAML file; if NULL, uses ./molevol_scripts/log_data/job_proc_weights.yml
+#' @param dir_job_results [chr] path to MolEvolvR job_results directory
+#' @param filepath [chr] path to save YAML file; if NULL, uses ./molevol_scripts/log_data/job_proc_weights.yml
 #'
 #' example: write_proc_medians_yml(
 #'   "/data/scratch/janani/molevolvr_out/",
 #'   "/data/scratch/janani/molevolvr_out/log_tbl.yml"
 #' )
-write_proc_medians_yml <- function(job_results_folder, filepath=NULL) {
+write_proc_medians_yml <- function(dir_job_results, filepath=NULL) {
   if (is.null(filepath)) {
     filepath <- file.path(common_root, "molevol_scripts", "log_data", "job_proc_weights.yml")
   }
 
-  medians <- get_proc_medians(job_results_folder)
+  medians <- get_proc_medians(dir_job_results)
   yaml::write_yaml(medians, filepath)
 }
 
-# molevolvr backend process weight list based on the median walltimes
-# for each process (proc)
-# example: get_proc_weights()
+#' Quickly get the runtime weights for MolEvolvR backend processes
+#'
+#' @param dir_job_results [chr] path to MolEvolvR job_results 
+#' directory
+#'
+#' @return [list] names: processes; values: median runtime (seconds)
+#' 
+#' example: get_proc_weights()
 get_proc_weights <- function(medians_yml_path=NULL) {
   if (is.null(medians_yml_path)) {
     medians_yml_path <- file.path(common_root, "molevol_scripts", "log_data", "job_proc_weights.yml")
@@ -165,7 +169,10 @@ get_proc_weights <- function(medians_yml_path=NULL) {
 
       proc_weights <- yaml::read_yaml(medians_yml_path)
     },
-    error=function(cond) {
+    # to avoid fatal errors in reading the proc weights yaml,
+    # some median process runtimes have been hardcoded based on
+    # the result of get_proc_medians() from Jan 2024
+    error = function(cond) {
       proc_weights <- list(
         "dblast" = 2810,
         "iprscan" = 1016,
@@ -175,6 +182,7 @@ get_proc_weights <- function(medians_yml_path=NULL) {
         "blast_clust" = 2,
         "clust2table" = 2
       )
+      proc_weights
     }
   )
 
@@ -276,17 +284,28 @@ plot_estimated_walltimes <- function() {
   }
   opts_power_set <- get_powerset(opts)
   est_walltimes <- list()
-  for (n_inputs in 1:20) {
+  for (i in 1:20) {
     est_walltimes <- append(
       x = est_walltimes,
       values = sapply(
         opts_power_set,
         FUN = function(advanced_opts) {
-          est_walltime <- advanced_opts2est_walltime(advanced_opts, n_inputs = n_inputs)
+          # for simplicity, assume the default number of homologus hits (100)
+          n_hits <- if ("homology_search" %in% advanced_opts) {
+            100
+          } else {
+            NULL
+          }
+          est_walltime <- advanced_opts2est_walltime(
+            advanced_opts,
+            n_inputs = i,
+            n_hits = n_hits,
+            verbose = TRUE
+          )
           names(est_walltime) <- paste0(advanced_opts, collapse = "_")
           est_walltime
         }
-      ) 
+      )
     )
   }
   # concat all results to their unique names
@@ -303,22 +322,21 @@ plot_estimated_walltimes <- function() {
     matrix(nrow = length(est_walltimes[[1]]), ncol = length(names(est_walltimes)))
   colnames(df_walltimes) <- names(est_walltimes)
   df_walltimes <- df_walltimes |> tibble::as_tibble()
-  # rm any col or powerset outcome without the "any" processes
-  col_idx_keep <- grep(pattern = "any$", x = names(df_walltimes))
+  # rm always col or powerset outcome without the "always" processes
+  col_idx_keep <- grep(pattern = "always$", x = names(df_walltimes))
   df_walltimes <- df_walltimes |>
     dplyr::select(col_idx_keep)
   # bind n_inputs
   df_walltimes <- df_walltimes |>
     dplyr::mutate(n_inputs = 1:20)
   df_walltimes <- tidyr::gather(df_walltimes, key = "advanced_opts", value = "est_walltime", -n_inputs)
-  # sec to min
+  # sec to hrs
   df_walltimes <- df_walltimes |>
-    dplyr::mutate(est_walltime = est_walltime / 60)
+    dplyr::mutate(est_walltime = est_walltime / 360)
   p <- ggplot2::ggplot(df_walltimes, ggplot2::aes(x = n_inputs, y = est_walltime, color = advanced_opts)) +
     ggplot2::geom_line() +
-    ggplot2::scale_y_log10() +
     ggplot2::labs(title = "MolEvolvR estimated runtimes",
         x = "Number of inputs",
-        y = "Estimated walltime (log_10(minutes))")
+        y = "Estimated walltime (hours)")
   return(p)
 }
