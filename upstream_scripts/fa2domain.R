c31b85c85f24e50177b3e9ad516b6e7fc68f5ac4
jakekrol <jdkrol351@gmail.com>
2024-02-18 21:14:19 -0700

    preliminary work on splitbydomain

--- upstream_scripts/fa2domain.R
+++ upstream_scripts/fa2domain.R
@@ -1,10 +1,55 @@
-suppressPackageStartupMessages(library("Biostrings"))
-source("/data/research/jravilab/molevol_scripts/upstream_scripts/00_submit_full.R")
+#!/usr/bin/env R
+### begin code from fa2domain.sh
+# adapted from original fa2domain.sh script
+# not sure if this is needed since there are 
+# two successive `cd` executions without any 
+# executions that were sensitive to the working directory
+# aside from the second `cd` call which could potentially
+# be relative
+dir_out <- Sys.getenv("SLURM_SUBMIT_DIR")
+setwd(dir_out)
+
 args <- commandArgs(trailingOnly = TRUE)
-get_sequences(args[1], dir = args[4], separate = FALSE, acc_file_path = "starting_accs.txt")
-in_fa <- readAAStringSet(args[1])
-in_ipr <- read.csv(args[2], sep = "\t", header = FALSE)
-out_file <- args[3]
+filepath_input <- args[1]
+type <- args[2]
+phylo <- args[3]
+dir <- args[4]
+prefix <- basename(filepath_input) |>
+  fs::path_ext_remove()
+setwd(dir)
+
+cmd_iprscan <- stringr::str_glue(
+  "/data/research/jravilab/molevol_scripts/upstream_scripts/04a_iprscan.sh ",
+  "{filepath_input} {prefix} {dir}"
+)
+
+
+file.create(
+  file.path(dir, stringr::str_glue("{prefix}.domains.fa"))
+)
+filepath_iprscan <- file.path(dir, stringr::str_glue("{prefix}.iprscan.tsv"))
+filepath_fa_domains <- file.path(dir, stringr::str_glue("{prefix}.domains.fa"))
+
+### end code from fa2domain.sh
+### begin fa2domain.R
+# converting previous args to variables available in combined script
+#args[1] <- filepath_input
+#args[2] <- file.path(dir, stringr::str_glue("{prefix}.iprscan.tsv"))
+#args[3] <- file.path(dir, stringr::str_glue("{prefix}.domains.fa"))
+#args[4] <- dir
+#args[5] <- phylo
+#args[6] <- type
+
+# new
+get_sequences(
+  filepath_input,
+  dir = dir,
+  separate = FALSE,
+  acc_file_path = "starting_accs.txt"
+)
+fasta <- readAAStringSet(filepath_input)
+df_iprscan <- read.csv(filepath_iprscan, sep = "\t", header = FALSE)
+filepath_fa_domains <- args[3]
 for (item in names(in_fa)) {
   accession <- unlist(strsplit(item, " "))[1]
   pfam_count <- 1
