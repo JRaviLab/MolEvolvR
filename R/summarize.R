4f2adc00f544c4b49924a806c3062de0748b0bc1
jakekrol <jdkrol351@gmail.com>
2024-03-08 13:49:55 -0700

    document and prevent warnings from summarize functions
    I was documenting the code to understand it. Now, I'm pushing the documentation
    
    - One minor change is that `select` was being used before in
    `elements2words` which lead to strange resulting string of
    'c( w1 w2 w-3)'. Now with pull, the output is much better: 'w1 w2 w-3'.
    
    Because the return value is always fed into words2wc, this fix is not
    essential but is better, imo.
    
    - Add Roxygen2 documentation to a few functions

--- R/summarize.R
+++ R/summarize.R
@@ -90,10 +90,18 @@ count_bycol <- function(prot = prot, column = "DomArch", min.freq = 1) {
   return(counts)
 }
 
-## Function to break up ELEMENTS to WORDS for DA and GC
+#' Break string ELEMENTS into WORDS for domain architecture (DA) and genomic 
+#' context (GC)
+#' @prot [dataframe]
+#' @column [string] column name
+#' @conversion_type [string] type of conversion: 'da2doms': domain architectures to 
+#' domains. 'gc2da' genomic context to domain architectures
+#' @return [string] with words delimited by spaces
+#' 
+#' example: tibble::tibble(DomArch = c("aaa+bbb", "a+b", "b+c", "b-c")) |> elements2words()
 elements2words <- function(prot, column = "DomArch", conversion_type = "da2doms") {
   z1 <- prot %>%
-    select(column) %>%
+    dplyr::pull(column) %>%
     str_replace_all("\\,", " ") %>%
     str_replace_all("\"", " ")
   switch(conversion_type,
@@ -125,10 +133,14 @@ elements2words <- function(prot, column = "DomArch", conversion_type = "da2doms"
   return(z3)
 }
 
-## Function to get WORD COUNTS [DOMAINS (DA) or DOMAIN ARCHITECTURES (GC)]
-## to be used after elements2words
-words2wc <- function(x) {
-  x %>%
+#' Get word counts (wc) [DOMAINS (DA) or DOMAIN ARCHITECTURES (GC)]
+#' @param string
+#' @return [tbl_df] table with 2 columns: 1) words & 2) counts/frequency
+#' example: tibble::tibble(DomArch = c("aaa+bbb", "a+b", "b+c", "b-c")) |> 
+#'   elements2words() |>
+#'   words2wc()
+words2wc <- function(string) {
+  df_word_count <- string %>%
     str_replace_all("   ", " ") %>%
     str_replace_all("  ", " ") %>%
     str_replace_all("  ", " ") %>%
@@ -154,6 +166,7 @@ words2wc <- function(x) {
 
     # filter(!grepl("\\*", words)) %>%			# Remove/Keep only Query
     arrange(-freq)
+    return(df_word_count)
 }
 ## Function to filter based on frequencies
 filter_freq <- function(x, min.freq) {
