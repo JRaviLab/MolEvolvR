5b1fd058a3ac22ebc349b3951c7c5aad28c5cf4b
David Mayer <david.mayer@cuanschutz.edu>
2024-08-20 11:02:57 -0600

    remove non-package repo contents - will replace with git submodule to molevol_scripts and transfer as needed to this repo

--- scripts/acc2tax/p_acc2tax.makefile
+++
@@ -1,5 +0,0 @@
-
-p_acc2tax: p_acc2tax.cpp
-	g++ -Wall p_acc2tax.cpp -lpthread -o p_acc2tax
-
-
--- unused/c2r.R
+++
@@ -1,6 +0,0 @@
-## To run Kalign and such
-
-## running C scripts within R
-# https://www.r-bloggers.com/three-ways-to-call-cc-from-r/
-# http://r-pkgs.had.co.nz/src.html
-
