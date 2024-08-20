03195b45c784682627f77cbec1c50ee16050a7a8
David Mayer <david.mayer@cuanschutz.edu>
2024-08-20 11:02:57 -0600

    remove non-package repo contents - will replace with git submodule to molevol_scripts and transfer as needed to this repo

--- upstream_scripts/00_submit_fa2domain.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#PBS -N submit_fa2domain
-#PBS -l nodes=1 :ppn 1
-qsub -v SCRIPT=${SCRIPT},INFILE=${INFILE},OTHERARGS=${OTHERARGS} /data/research/jravilab/molevol_scripts/upstream_scripts/00_wrapper_fa2domain.sh
