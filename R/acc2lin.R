919d60bbc075c81e3ba136768d8f7b98a79a84cc
David Mayer <david.mayer@cuanschutz.edu>
2024-10-08 10:17:52 -0600

    update dependent .Rd referencing downloadAssemblySummary()

--- R/acc2lin.R
+++ R/acc2lin.R
@@ -73,7 +73,7 @@ addLineage <- function(df, acc_col = "AccNum", assembly_path,
 #'
 #' @param accessions Character vector of protein accessions
 #' @param assembly_path String of the path to the assembly_summary path
-#' This file can be generated using the "DownloadAssemblySummary()" function
+#' This file can be generated using the \link[MolEvolvR]{downloadAssemblySummary} function
 #' @param lineagelookup_path String of the path to the lineage lookup file
 #' (taxid to lineage mapping). This file can be generated using the
 #' @param ipgout_path Path to write the results of the efetch run of the accessions
@@ -179,7 +179,7 @@ efetchIPG <- function(accnums, out_path, plan = "sequential", ...) {
 #' ipg database. The protein accession in 'accessions' should be contained in this
 #' file
 #' @param assembly_path String of the path to the assembly_summary path
-#' This file can be generated using the "DownloadAssemblySummary()" function
+#' This file can be generated using the \link[MolEvolvR]{downloadAssemblySummary} function
 #' @param lineagelookup_path String of the path to the lineage lookup file
 #' (taxid to lineage mapping). This file can be generated using the
 #' "create_lineage_lookup()" function
