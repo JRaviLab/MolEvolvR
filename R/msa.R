1a68176b9e765006391af63b4a0c29a62d6888cd
David Mayer <david.mayer@cuanschutz.edu>
2024-10-31 09:45:11 -0600

    kalign seems to come from an external GitHub repo -- removing from namespace

--- R/msa.R
+++ R/msa.R
@@ -196,7 +196,7 @@ createMSA_PDF <- function(fasta_path, out_path = NULL,
 #' will be saved.
 #'
 #' @importFrom Biostrings readAAStringSet
-#' @importFrom stats kalign
+#' 
 #' @return A list containing the alignment object and the output file path.
 #' @export
 #'
